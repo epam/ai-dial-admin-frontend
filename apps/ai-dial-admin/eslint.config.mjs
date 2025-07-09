f7ec3cfc5f39cb42e8d3e657c6df24404ce8fa07
Polina <palina_kryvasheyeva@epam.com>
2025-07-09 13:17:48 +0200

    chore: fix lint configuration

--- apps/ai-dial-admin/eslint.config.mjs
+++ apps/ai-dial-admin/eslint.config.mjs
@@ -1,8 +1,5 @@
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  {
-    ignores: ['.next/**/*', '**/**.config.js', '**/**.config.mjs', '**/jest.config.ts', '**/setup-jest.js'],
-  },
   ...baseConfig,
 ];
