afefeb6999eef8dc4c6545f50d5f9c4e1aa963fc
Polina <palina_kryvasheyeva@epam.com>
2025-07-11 09:34:10 +0200

    feat(application publication): selected view

--- apps/ai-dial-admin/src/components/PublicationView/BasePublicationProperties/BasePublicationProperties.tsx
+++ apps/ai-dial-admin/src/components/PublicationView/BasePublicationProperties/BasePublicationProperties.tsx
@@ -1,16 +1,18 @@
 import { FC, ReactNode } from 'react';
 
 import classNames from 'classnames';
+import { IconExternalLink } from '@tabler/icons-react';
 
 import LabeledText from '@/src/components/Common/LabeledText/LabeledText';
 import { PublicationsI18nKey } from '@/src/constants/i18n';
 import { ACTION_I18N_KEYS } from '@/src/constants/publications';
 import { useI18n } from '@/src/locales/client';
-import { ActionType, Publication } from '@/src/models/dial/publications';
+import { ActionType, ApplicationPublication, Publication } from '@/src/models/dial/publications';
 import { formatDateTimeToLocalString } from '@/src/utils/formatting/date';
 import { removeTrailingSlash } from '@/src/utils/files/path';
 import { getActionClass } from '@/src/utils/publications';
 import BasePublicationPermissions from './BasePublicationPermissions';
+import { ApplicationRoute } from '@/src/types/routes';
 
 interface Props {
   publication: Publication;
@@ -18,6 +20,8 @@ interface Props {
 }
 
 const BasePublicationProperties: FC<Props> = ({ publication, children }) => {
+  // TODO: add link to runner if available
+  const applicationResource = (publication as ApplicationPublication).applicationResources?.[0];
   const t = useI18n() as (stringToTranslate: string) => string;
   const indicatorClassNames = classNames(
     'flex w-2 h-2 mr-1 rounded no-user-select',
@@ -28,12 +32,29 @@ const BasePublicationProperties: FC<Props> = ({ publication, children }) => {
     <div className="h-full flex flex-col pt-3 divide-y divide-primary w-full" data-testid={'publication-header'}>
       <div className="flex flex-col sm:flex-row gap-8">
         <div className="flex flex-row gap-8">
-          <LabeledText label={t(PublicationsI18nKey.Action)}>
-            <p className="flex items-center">
-              <span className={indicatorClassNames} />
-              {t(ACTION_I18N_KEYS[publication.action])}
-            </p>
-          </LabeledText>
+          {applicationResource ? (
+            <LabeledText label={t(PublicationsI18nKey.Runner)}>
+              <p
+                className="flex items-center cursor-pointer"
+                onClick={() =>
+                  window.open(
+                    `${ApplicationRoute.ApplicationRunners}/${encodeURIComponent(applicationResource.displayName)}`,
+                    '_blank',
+                  )
+                }
+              >
+                <span> {applicationResource.displayName}</span>
+                <IconExternalLink width={16} height={16} className="ml-2 text-secondary" />
+              </p>
+            </LabeledText>
+          ) : (
+            <LabeledText label={t(PublicationsI18nKey.Action)}>
+              <p className="flex items-center">
+                <span className={indicatorClassNames} />
+                {t(ACTION_I18N_KEYS[publication.action])}
+              </p>
+            </LabeledText>
+          )}
           <LabeledText label={t(PublicationsI18nKey.Author)} text={publication.author} />
         </div>
         <div className="flex flex-col sm:flex-row gap-8">
