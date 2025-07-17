da5d903dee417fe434d5fdff3a8e1780abbf9345
Polina <palina_kryvasheyeva@epam.com>
2025-07-17 12:02:52 +0200

    fix(application publication): fix styles and files view

--- apps/ai-dial-admin/src/utils/files/get-extension.ts
+++ apps/ai-dial-admin/src/utils/files/get-extension.ts
@@ -1,5 +1,5 @@
 export const getNameExtensionFromFile = (input: string): { name: string; extension: string } => {
-  const lastUnderscoreIndex = input.lastIndexOf('.');
+  const lastUnderscoreIndex = (input || '').lastIndexOf('.');
 
   if (lastUnderscoreIndex === -1) {
     return { name: input, extension: '' };
