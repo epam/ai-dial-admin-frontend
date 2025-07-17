b665176b7ea4de6305141d9280388146d67333bf
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-07-17 17:54:42 +0200

    fix(logo): logo position and sizes (Issue #125) (#126)

--- apps/ai-dial-admin/src/components/Header/Header.tsx
+++ apps/ai-dial-admin/src/components/Header/Header.tsx
@@ -25,7 +25,7 @@ const Header: FC<Props> = ({ isEnableAuth }) => {
   }, [sidebarOpen]);
 
   return (
-    <header className={classNames('z-40 flex w-full border-b border-tertiary bg-layer-3')}>
+    <header className={classNames('z-40 flex w-full border-b border-tertiary bg-layer-3 relative justify-between')}>
       <div className="flex">
         <Button
           cssClass="py-3 px-5 border-r border-r-tertiary focus-within:outline-offset-[-1px] focus:text-accent-primary hover:text-accent-primary"
