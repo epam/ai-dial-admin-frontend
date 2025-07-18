d38544aac35fafb9b3a355225360d660f476bf69
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-07-18 13:03:17 +0200

    fix(endpoint): use adapter baseEndpoint (#133)

--- apps/ai-dial-admin/src/components/ModelView/ModelProperties/utils.ts
+++ apps/ai-dial-admin/src/components/ModelView/ModelProperties/utils.ts
@@ -1,10 +1,10 @@
 import { DialModel, DialModelType } from '@/src/models/dial/model';
+import { DialAdapter } from '@/src/models/dial/adapter';
 
-const deploymentsPrefix = '/deployments/';
-
-export const splitEndpoint = (model: DialModel): [string, string] => {
+export const splitEndpoint = (model: DialModel, adapters: DialAdapter[]): [string, string] => {
   const postfix = model.type === DialModelType.Chat ? '/chat/completions' : '/embeddings';
   const endpoint = model.endpoint?.split(postfix)[0];
-  const adapter = adapters.find((a) => `${endpoint}/`?.startsWith(a.baseEndpoint || ''));
-  return [prefix, postfix];
+  const adapter = adapters.find((a) => endpoint?.startsWith(a.baseEndpoint || ''));
+
+  return [adapter?.baseEndpoint || '', postfix];
 };
