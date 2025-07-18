d57c2bf3f27352c674392520a7f7d5a0186ace7e
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-07-18 10:36:52 +0200

    fix(endpoint): empty state (Issue #106) (#129)

--- apps/ai-dial-admin/src/components/ModelView/ModelProperties/utils.ts
+++ apps/ai-dial-admin/src/components/ModelView/ModelProperties/utils.ts
@@ -2,10 +2,9 @@ import { DialModel, DialModelType } from '@/src/models/dial/model';
 
 const deploymentsPrefix = '/deployments/';
 
-export const splitEndpoint = (model: DialModel): [string, string, string] => {
+export const splitEndpoint = (model: DialModel): [string, string] => {
   const postfix = model.type === DialModelType.Chat ? '/chat/completions' : '/embeddings';
   const prefix = `${model.endpoint?.split(deploymentsPrefix)[0] || ''}${deploymentsPrefix}`;
-  const endpoint = model.endpoint?.replace(postfix, '').replace(prefix, '') || '';
 
-  return [prefix, endpoint, postfix];
+  return [prefix, postfix];
 };
