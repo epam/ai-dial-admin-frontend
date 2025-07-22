6b0bb4cd8238e45e8cd67a4637c45681dc0bb352
Siarhei Simanovich <Siarhei_Simanovich@epam.com>
2025-07-22 22:23:10 +0200

    Add tests

---
+++ apps/ai-dial-admin/src/components/InterceptorTemplates/View/Interceptors/Interceptors.spec.tsx
@@ -0,0 +1,44 @@
+import { describe, test, vi, expect, beforeEach, Mock } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+
+import { getInterceptorsList } from '@/src/app/[lang]/interceptors/actions';
+
+import Interceptors from '@/src/components/InterceptorTemplates/View/Interceptors/Interceptors';
+
+vi.mock('@/src/app/[lang]/interceptors/actions', () => ({
+  getInterceptorsList: vi.fn(),
+}));
+
+const interceptors = [
+  { name: 'Interceptor-1', description: 'Description 1' },
+  { name: 'Interceptor-2', description: 'Description 2' },
+];
+
+describe('Interceptors', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('should render no data when interceptors list is empty', () => {
+    render(<Interceptors interceptorList={[]} />);
+
+    expect(screen.getByText('Entities.NoInterceptors')).toBeInTheDocument();
+  });
+
+  test('should render no data when interceptors from BE', () => {
+    (getInterceptorsList as Mock).mockResolvedValue([]);
+    render(<Interceptors interceptorList={['name']} />);
+
+    expect(screen.getByText('Entities.NoInterceptors')).toBeInTheDocument();
+  });
+
+  test('should render grid with interceptors', async () => {
+    (getInterceptorsList as Mock).mockResolvedValue(interceptors);
+
+    render(<Interceptors interceptorList={['Interceptor-1']} />);
+
+    await waitFor(() => {
+      expect(screen.getByRole('table')).toBeInTheDocument();
+    });
+  });
+});
