65151c16c0d71dd391fd103349f68b3bc6c0cb62
Polina <palina_kryvasheyeva@epam.com>
2025-08-06 14:56:31 +0200

    fix: add validation for model endpoint alias (Issue #276)

--- apps/ai-dial-admin/src/components/EntityView/EntityViewHeaderButtons.tsx
+++ apps/ai-dial-admin/src/components/EntityView/EntityViewHeaderButtons.tsx
@@ -13,7 +13,7 @@ import AddVersionModal from '@/src/components/Common/AddVersionModal/AddVersionM
 import Button from '@/src/components/Common/Button/Button';
 import ConfirmationModal from '@/src/components/Common/ConfirmationModal/ConfirmationModal';
 import Switch from '@/src/components/Common/Switch/Switch';
-import { isValidEntity } from '@/src/components/EntityListView/CreateEntity/utils';
+import { isValidEntity } from '@/src/utils/validation/is-valid-entity';
 import { deleteModalTitleMap } from '@/src/components/EntityListView/constants';
 import { getEntityPath } from '@/src/utils/open-in-new-tab';
 import { showEditorErrorNotifications } from '@/src/components/JSONEditor/JSONEditor.utils';
