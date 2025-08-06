b916e26db914a8ce31fa8aa53e7513b96824fae8
Polina <palina_kryvasheyeva@epam.com>
2025-08-06 17:26:44 +0200

    chore: remove Addons and Assistants

--- apps/ai-dial-admin/src/components/InterceptorsList/InterceptorView.spec.tsx
+++ apps/ai-dial-admin/src/components/InterceptorsList/InterceptorView.spec.tsx
@@ -5,12 +5,7 @@ import InterceptorView from './InterceptorView';
 describe('Interceptor View', () => {
   test('Should render successfully', () => {
     const { baseElement } = render(
-      <InterceptorView
-        names={[]}
-        originalInterceptor={{ name: 'interceptor' }}
-        applications={[]}
-        models={[]}
-      />,
+      <InterceptorView names={[]} originalInterceptor={{ name: 'interceptor' }} applications={[]} models={[]} />,
     );
     expect(baseElement).toBeTruthy();
   });
