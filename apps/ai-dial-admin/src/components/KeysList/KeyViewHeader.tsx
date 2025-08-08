bb583c6a3a12307cf3b9fe118e02c3e1aaf35a5f
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-08-08 10:43:25 +0200

    feature: change titles for create time and update time ( Issue #68) (#302)

--- apps/ai-dial-admin/src/components/KeysList/KeyViewHeader.tsx
+++ apps/ai-dial-admin/src/components/KeysList/KeyViewHeader.tsx
@@ -4,27 +4,30 @@ import { DialKey } from '@/src/models/dial/key';
 import { formatDateTimeToLocalString } from '@/src/utils/formatting/date';
 import LabeledText from '@/src/components/Common/LabeledText/LabeledText';
 import KeyViewStatus from './KeyStatus/KeyViewStatus';
+import { EntitiesI18nKey, KeysI18nKey } from '@/src/constants/i18n';
+import { useI18n } from '@/src/locales/client';
 
 interface Props {
   selectedKey: DialKey;
 }
 
 const KeyViewHeader: FC<Props> = ({ selectedKey }) => {
+  const t = useI18n();
   return (
     <div className="flex flex-row gap-10 w-full">
       <LabeledText
-        label="Creation Time"
+        label={t(EntitiesI18nKey.CreatedAt)}
         text={selectedKey.createdAt ? formatDateTimeToLocalString(selectedKey.createdAt) : ''}
       />
       <LabeledText
-        label="Key generation Time"
+        label={t(KeysI18nKey.KeyGenerationTime)}
         text={selectedKey.keyGeneratedAt ? formatDateTimeToLocalString(selectedKey.keyGeneratedAt) : ''}
       />
       <LabeledText
-        label="Expiration Time"
+        label={t(KeysI18nKey.ExpirationTime)}
         text={selectedKey.expiresAt ? formatDateTimeToLocalString(selectedKey.expiresAt) : ''}
       />
-      <LabeledText label="Status">
+      <LabeledText label={t(KeysI18nKey.StatusTitle)}>
         <KeyViewStatus data={selectedKey} />
       </LabeledText>
     </div>
