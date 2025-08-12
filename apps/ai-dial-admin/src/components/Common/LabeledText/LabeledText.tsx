92e99a544d55e58378b7637828afdcda37b13e74
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-08-12 10:00:03 +0200

    feature: unification properties  (Issue #282) (#321)

--- apps/ai-dial-admin/src/components/Common/LabeledText/LabeledText.tsx
+++ apps/ai-dial-admin/src/components/Common/LabeledText/LabeledText.tsx
@@ -1,21 +1,27 @@
 import { FC, ReactNode } from 'react';
 
+import CopyButton from '@/src/components/Common/CopyButton/CopyButton';
+
 interface Props {
   label: string;
   text?: string;
+  copyButton?: boolean;
   children?: ReactNode;
 }
 
-const LabeledText: FC<Props> = ({ label, text, children }) => {
+const LabeledText: FC<Props> = ({ label, text, children, copyButton }) => {
   return (
     <div className="flex flex-col max-w-[200px]">
       <label className="tiny mb-2 text-secondary">{label}</label>
       {children ? (
         children
       ) : (
-        <p className="flex-inline truncate" title={text}>
-          {text}
-        </p>
+        <div className="flex flex-row">
+          <p className="flex-1 min-w-0 truncate mr-2" title={text}>
+            {text}
+          </p>
+          {copyButton && <CopyButton field={text} />}
+        </div>
       )}
     </div>
   );
