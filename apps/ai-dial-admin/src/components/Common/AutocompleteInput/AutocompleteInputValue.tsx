c34fd039cba6e9a3ca46e1c171b13d2a88e7db06
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-08-13 09:57:04 +0200

    fix: remove padding for empty value (Issue #301) (#340)

--- apps/ai-dial-admin/src/components/Common/AutocompleteInput/AutocompleteInputValue.tsx
+++ apps/ai-dial-admin/src/components/Common/AutocompleteInput/AutocompleteInputValue.tsx
@@ -10,22 +10,20 @@ interface Props {
 
 const AutocompleteInputValue: FC<Props> = ({ selectedItems }) => {
   return (
-    <ul
-      className={classNames(
-        'flex-row items-center truncate flex-wrap',
-        selectedItems && selectedItems.length && 'flex gap-x-2 gap-y-1',
-      )}
-    >
-      {selectedItems?.map((selectedItem) => (
-        <li key={selectedItem} className="tiny bg-layer-3 rounded p-1 border border-primary max-w-[200px] truncate">
-          <Tooltip triggerClassName="flex-1 min-w-0" contentClassName="truncate" tooltip={selectedItem}>
-            <button aria-label="autocomplete-action" type="button" className="truncate w-full">
-              {selectedItem}
-            </button>
-          </Tooltip>
-        </li>
-      ))}
-    </ul>
+    selectedItems &&
+    !!selectedItems.length && (
+      <ul className={classNames('flex-row items-center truncate flex-wrap', 'flex gap-x-2 gap-y-1')}>
+        {selectedItems?.map((selectedItem) => (
+          <li key={selectedItem} className="tiny bg-layer-3 rounded p-1 border border-primary max-w-[200px] truncate">
+            <Tooltip tooltip={selectedItem}>
+              <button aria-label="autocomplete-action" type="button" className="truncate w-full">
+                {selectedItem}
+              </button>
+            </Tooltip>
+          </li>
+        ))}
+      </ul>
+    )
   );
 };
 
