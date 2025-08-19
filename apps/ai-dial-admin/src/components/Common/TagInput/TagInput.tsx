b3c6b678da0eccadd421f516f2de58751425b8df
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-08-19 15:26:17 +0200

    fix: roles limit placeholder (Issue #398, #399) (#405)

--- apps/ai-dial-admin/src/components/Common/TagInput/TagInput.tsx
+++ apps/ai-dial-admin/src/components/Common/TagInput/TagInput.tsx
@@ -6,8 +6,6 @@ import classNames from 'classnames';
 
 import ErrorText from '@/src/components/Common/ErrorText/ErrorText';
 import Field from '@/src/components/Common/Field/Field';
-import { PlaceholderI18nKey } from '@/src/constants/i18n';
-import { useI18n } from '@/src/locales/client';
 import Tag from './Tag';
 
 interface Props {
@@ -31,8 +29,6 @@ const TagInput: FC<Props> = ({
   invalid,
   onChange,
 }) => {
-  const t = useI18n();
-
   const containerRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -92,7 +88,7 @@ const TagInput: FC<Props> = ({
             onChange={(e) => setInputValue(e.target.value)}
             onKeyDown={handleKeyDown}
             className={classNames('outline-none border-none w-full min-w-[100px] flex-1 p-1')}
-            placeholder={placeholder || t(PlaceholderI18nKey.TagsInput)}
+            placeholder={placeholder}
           />
         </div>
       </div>
