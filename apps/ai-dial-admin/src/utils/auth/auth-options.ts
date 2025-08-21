25cfcb6bb85df70bbe769f751f5472ff253663ba
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-08-21 10:29:37 +0200

    chore: add NEXTAUTH_DEBUG for next-auth (#419)
    Co-authored-by: Siarhei Simanovich <siarhei_simanovich@epam.com>

--- apps/ai-dial-admin/src/utils/auth/auth-options.ts
+++ apps/ai-dial-admin/src/utils/auth/auth-options.ts
@@ -6,6 +6,7 @@ import { authProviders } from './auth-providers';
 export const authOptions = {
   providers: authProviders,
   cookies,
+  debug: process.env.NEXTAUTH_DEBUG,
   callbacks,
   session: {
     strategy: 'jwt',
