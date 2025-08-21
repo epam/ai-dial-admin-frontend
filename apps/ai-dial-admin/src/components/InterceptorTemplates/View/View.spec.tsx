9cd148de2b8381a4d57ab153638a4518b6649b58
Polina <palina_kryvasheyeva@epam.com>
2025-08-21 12:16:50 +0200

    chore: review i18n keys for entity fields, error, placeholders

--- apps/ai-dial-admin/src/components/InterceptorTemplates/View/View.spec.tsx
+++ apps/ai-dial-admin/src/components/InterceptorTemplates/View/View.spec.tsx
@@ -5,6 +5,7 @@ import { InterceptorTemplate } from '@/src/models/interceptor-template';
 import { ApplicationRoute } from '@/src/types/routes';
 
 import View from './View';
+import { ButtonsI18nKey, EntityFieldsI18nKey, FeaturesI18nKey } from '@/src/constants/i18n';
 
 const template: InterceptorTemplate = {
   name: 'test-template',
@@ -18,11 +19,11 @@ describe('View', () => {
   it('Should render correctly', () => {
     render(<View route={ApplicationRoute.InterceptorTemplates} template={template} />);
 
-    expect(screen.getByRole('button', { name: 'Buttons.Delete' })).toBeInTheDocument();
-    expect(screen.getByRole('textbox', { name: 'CreateEntity.completionEndpoint.title' })).toHaveValue(
+    expect(screen.getByRole('button', { name: ButtonsI18nKey.Delete })).toBeInTheDocument();
+    expect(screen.getByRole('textbox', { name: EntityFieldsI18nKey.completionEndpoint })).toHaveValue(
       template.completionEndpoint,
     );
-    expect(screen.getByRole('textbox', { name: 'CreateEntity.configurationEndpoint.title' })).toHaveValue(
+    expect(screen.getByRole('textbox', { name: FeaturesI18nKey.configurationEndpoint })).toHaveValue(
       template.configurationEndpoint,
     );
   });
