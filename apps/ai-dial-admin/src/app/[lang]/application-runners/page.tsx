8f50503e3dd8ff65b6d8c5db3ab6a99561fea436
Polina <palina_kryvasheyeva@epam.com>
2025-08-21 18:02:37 +0200

    fix build

--- apps/ai-dial-admin/src/app/[lang]/application-runners/page.tsx
+++ apps/ai-dial-admin/src/app/[lang]/application-runners/page.tsx
@@ -2,7 +2,7 @@ import { cookies, headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import { applicationRunnersApi } from '@/src/app/api/api';
-import ApplicationRunnersList from '@/src/components/ApplicationRunners/List/ApplicationRunnersList';
+import ApplicationRunnersList from '@/src/components/ApplicationRunners/List/List';
 import { DialApplicationScheme } from '@/src/models/dial/application';
 import { getUserToken } from '@/src/utils/auth/auth-request';
 import { getIsEnableAuthToggle } from '@/src/utils/env/get-auth-toggle';
