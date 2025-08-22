02020033ef5008cd2d92cab1816d18277ad855ac
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-08-22 10:11:33 +0200

    fix: route fields for audit (#424)

--- apps/ai-dial-admin/src/constants/i18n.ts
+++ apps/ai-dial-admin/src/constants/i18n.ts
@@ -198,7 +198,6 @@ export enum ButtonsI18nKey {
   Unpublish = 'Buttons.Unpublish',
   Rotate = 'Buttons.Rotate',
   ReviewStructure = 'Buttons.ReviewStructure',
-  CompareChanges = 'Buttons.CompareChanges',
   Refresh = 'Buttons.Refresh',
   Rollback = 'Buttons.Rollback',
   Back = 'Buttons.Back',
@@ -344,10 +343,6 @@ export enum TabsI18nKey {
   Conversations = 'Tabs.Conversations',
 }
 
-export enum EditorI18nKey {
-  ErrorLine = 'Editor.ErrorLine',
-}
-
 export enum InterceptorsI18nKey {
   Add = 'Interceptors.Add',
 }
@@ -390,12 +385,7 @@ export enum TelemetryI18nKey {
 
 export enum RoutesI18nKey {
   AddPaths = 'Routes.AddPaths',
-  RewritePath = 'Routes.RewritePath',
   Output = 'Routes.Output',
-  Response = 'Routes.Response',
-  RequiredProperty = 'Routes.RequiredProperty',
-  InvalidPath = 'Routes.InvalidPath',
-  InvalidStatus = 'Routes.InvalidStatus',
 }
 
 export enum PublicationsI18nKey {
@@ -405,7 +395,7 @@ export enum PublicationsI18nKey {
   PromptUnpublishDeclineModalTitle = 'Publications.Prompt.UnpublishDeclineModalTitle',
   PromptPublishApproveDescription = 'Publications.Prompt.PublishApproveDescription',
   PromptUnpublishApproveDescription = 'Publications.Prompt.UnpublishApproveDescription',
-  DeclineReason = 'Publications.Prompt.ReasonTitle',
+  DeclineReason = 'Publications.ReasonTitle',
   DeclineReasonPlaceholder = 'Publications.Prompt.ReasonPlaceholder',
   FilePublishApproveModalTitle = 'Publications.Files.PublishApproveModalTitle',
   FilePublishDeclineModalTitle = 'Publications.Files.PublishDeclineModalTitle',
@@ -420,13 +410,15 @@ export enum PublicationsI18nKey {
   ApplicationPublishApproveDescription = 'Publications.Applications.PublishApproveDescription',
   ApplicationUnpublishApproveDescription = 'Publications.Applications.UnpublishApproveDescription',
   OpenPrompt = 'Publications.Prompt.OpenPrompt',
-  ActionPublish = 'Publications.ActionPublish',
-  ActionUnpublish = 'Publications.ActionUnpublish',
   Action = 'Publications.Action',
   Runner = 'Publications.Runner',
   Author = 'Publications.Author',
   FolderStorage = 'Publications.FolderStorage',
   FilesListTitle = 'Publications.Files.FilesListTitle',
+}
+
+export enum CompareI18nKey {
+  CompareChanges = 'Compare.CompareChanges',
 }
 
 export enum PromptsI18nKey {
@@ -598,6 +590,10 @@ export enum ErrorI18nKey {
   MinMaxLength = 'Error.MinMaxLength',
   KeyValueExists = 'Error.KeyValueExists',
   ForbiddenChars = 'Error.ForbiddenChars',
+  RequiredProperty = 'Error.RequiredProperty',
+  InvalidPath = 'Error.InvalidPath',
+  InvalidStatus = 'Error.InvalidStatus',
+  EditorErrorLine = 'Error.EditorErrorLine',
 }
 
 export enum SourceI18nKey {
@@ -666,6 +662,8 @@ export enum EntityFieldsI18nKey {
   displayVersion = 'EntityFields.displayVersion',
   adapter = 'EntityFields.adapter',
   iconUrl = 'EntityFields.iconUrl',
+  rewritePath = 'EntityFields.rewritePath',
+  response = 'EntityFields.response',
   description = 'EntityFields.description',
   forwardAuthToken = 'EntityFields.forwardAuthToken',
   features = 'EntityFields.features',
