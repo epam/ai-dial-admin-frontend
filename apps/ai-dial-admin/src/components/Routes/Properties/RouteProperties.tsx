cee1bbb66e20cdac2ca04169b63987f63ac5f91d
Polina <palina_kryvasheyeva@epam.com>
2025-08-26 17:27:39 +0200

    fix: empty value in input

--- apps/ai-dial-admin/src/components/Routes/Properties/RouteProperties.tsx
+++ apps/ai-dial-admin/src/components/Routes/Properties/RouteProperties.tsx
@@ -52,14 +52,14 @@ const RouteProperties: FC<Props> = ({ route, readonly, isAppRoute, updateRoute }
   }, [permissionsItems, route]);
 
   const onChangeName = useCallback(
-    (name: string) => {
+    (name?: string) => {
       updateRoute({ ...route, name });
     },
     [route, updateRoute],
   );
 
   const onChangeDisplayName = useCallback(
-    (displayName: string) => {
+    (displayName?: string) => {
       updateRoute({ ...route, displayName });
     },
     [route, updateRoute],
