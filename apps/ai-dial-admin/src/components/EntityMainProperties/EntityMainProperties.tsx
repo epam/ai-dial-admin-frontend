f62da78d6bdf56b48e15fb8f50ba79f5f9727962
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-09-02 13:50:20 +0200

    fix: model validation when model's display name is empty (#515)

--- apps/ai-dial-admin/src/components/EntityMainProperties/EntityMainProperties.tsx
+++ apps/ai-dial-admin/src/components/EntityMainProperties/EntityMainProperties.tsx
@@ -77,17 +77,6 @@ const EntityMainProperties: FC<Props> = ({
   );
 
   useEffect(() => {
-    if (view === ApplicationRoute.Models || view === ApplicationRoute.Applications) {
-      dispatch({
-        type: ValidationActionType.SetField,
-        field: 'displayName',
-        isValid: !!entity.displayName,
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [view]);
-
-  useEffect(() => {
     if (view === ApplicationRoute.Models) {
       dispatch({ type: ValidationActionType.SetField, field: 'displayVersion', isValid: !versionError });
     }
@@ -123,6 +112,7 @@ const EntityMainProperties: FC<Props> = ({
           onChange={onChangeDisplayName}
           invalid={!!displayNameError}
           items={uniq(names)}
+          optional={true}
         />
 
         {view === ApplicationRoute.Models && (
