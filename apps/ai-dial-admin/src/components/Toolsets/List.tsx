8b578219dc609cc6cd04babbb9cdead3d1c7850a
Polina <palina_kryvasheyeva@epam.com>
2025-09-02 17:21:28 +0200

    feat: [Toolsets] Listing Page

--- apps/ai-dial-admin/src/components/Toolsets/List.tsx
+++ apps/ai-dial-admin/src/components/Toolsets/List.tsx
@@ -2,7 +2,7 @@
 
 import { FC } from 'react';
 
-import { AUTHOR_COLUMN, SIMPLE_ENTITY_COLUMNS } from '@/src/constants/grid-columns/grid-columns';
+import { AUTHOR_COLUMN, SIMPLE_ENTITY_COLUMNS, TOPIC_COLUMN } from '@/src/constants/grid-columns/grid-columns';
 import BaseEntityList from '@/src/components/EntityListView/EntityListView';
 import { ApplicationRoute } from '@/src/types/routes';
 import { DialToolset } from '@/src/models/dial/toolset';
@@ -22,7 +22,7 @@ const ToolsetsList: FC<Props> = ({ data }) => {
 
   return (
     <BaseEntityList
-      baseColumns={[...SIMPLE_ENTITY_COLUMNS, AUTHOR_COLUMN]}
+      baseColumns={[...SIMPLE_ENTITY_COLUMNS, AUTHOR_COLUMN, TOPIC_COLUMN]}
       names={names}
       data={data}
       route={ApplicationRoute.Toolsets}
