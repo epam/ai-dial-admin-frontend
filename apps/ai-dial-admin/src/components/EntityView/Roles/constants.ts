5b44e857748634d74e08ba89fece1f1181ad4da3
Yauheni Osipau <yauheni_osipau@epam.com>
2025-09-09 15:36:11 +0300

    added comment

--- apps/ai-dial-admin/src/components/EntityView/Roles/constants.ts
+++ apps/ai-dial-admin/src/components/EntityView/Roles/constants.ts
@@ -1,5 +1,6 @@
 import { RolesI18nKey } from '@/src/constants/i18n';
 
+// max values for BE
 export const NO_LIMITS_VALUE = '9223372036854775807';
 export const NO_LIMITS_ACCEPTED_USERS = '2147483647';
 
