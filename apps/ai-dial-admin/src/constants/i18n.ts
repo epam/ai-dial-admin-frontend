dd043c21e1a9c22bd11a804d1cec573da7c50838
Siarhei Simanovich <Siarhei_Simanovich@epam.com>
2025-09-11 17:21:48 +0200

    feat(audit): support source paramenters in logs (Issue #636)

--- apps/ai-dial-admin/src/constants/i18n.ts
+++ apps/ai-dial-admin/src/constants/i18n.ts
@@ -782,6 +782,10 @@ export enum EntityFieldsI18nKey {
   dependencies = 'EntityFields.dependencies',
   completionEndpoint = 'EntityFields.completionEndpoint',
   container = 'EntityFields.container',
+  $type = 'EntityFields.SourceType',
+  adapterName = 'EntityFields.SourceAdapter',
+  runnerName = 'EntityFields.SourceInterceptorTemplate',
+  containerId = 'EntityFields.SourceContainer',
 }
 
 export enum ToolsetI18nKey {
