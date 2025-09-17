71790b80bc0ad7bd58275c0fff536f89f44657c0
Siarhei Simanovich <siarhei_simanovich@epam.com>
2025-09-17 11:20:10 +0200

    feat(usage-log): release usage log feature (Issue #138, #140) (#674)

--- apps/ai-dial-admin/src/server/telemetry-api.ts
+++ apps/ai-dial-admin/src/server/telemetry-api.ts
@@ -5,14 +5,9 @@ import { API } from './api';
 import { BaseApi } from './base-api';
 
 export const DASHBOARD_URL = `${API}/metrics/datasets/dial_analytics_realtime/data`;
-export const USAGE_LOG_URL = `${API}/usageLog`;
 
 export class TelemetryApi extends BaseApi {
   getDashboardData(query: TelemetryQuery, token: JWT | null): Promise<ServerActionResponse> {
     return this.postAction(DASHBOARD_URL, query, token);
-  }
-
-  getUsageLog(token: JWT | null): Promise<any[] | null> {
-    return this.get(USAGE_LOG_URL, token);
   }
 }
