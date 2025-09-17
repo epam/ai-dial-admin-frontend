abbc681028d4cd64c376b6485bfeeb544f963b67
Siarhei Simanovich <Siarhei_Simanovich@epam.com>
2025-09-17 11:04:20 +0200

    feat(usage-log) release usage log feature (Issue #138, #140)

--- apps/ai-dial-admin/src/app/[lang]/usage-log/page.tsx
+++ apps/ai-dial-admin/src/app/[lang]/usage-log/page.tsx
@@ -2,35 +2,20 @@ import { cookies, headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import { ApplicationRoute } from '@/src/types/routes';
-import { telemetryApi } from '@/src/app/api/api';
 import { SIGN_IN_LINK } from '@/src/constants/auth';
-import { logger } from '@/src/server/logger';
 import { getUserToken } from '@/src/utils/auth/auth-request';
 import { getIsInvalidSession } from '@/src/utils/auth/is-valid-session';
 import { getIsEnableAuthToggle } from '@/src/utils/env/get-auth-toggle';
 
 import UsageLog from '@/src/components/UsageLog/UsageLog';
-import Page403 from '@/src/components/Page403/Page403';
-
-export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  let data: any[] | null = [];
   const isEnableAuth = getIsEnableAuthToggle();
   const token = await getUserToken(isEnableAuth, headers(), cookies());
   const isInvalidSession = await getIsInvalidSession(isEnableAuth, token);
 
   if (isInvalidSession) {
     return redirect(SIGN_IN_LINK);
-  }
-
-  try {
-    data = await telemetryApi.getUsageLog(token);
-    if (data === void 0) {
-      return <Page403 />;
-    }
-  } catch (e) {
-    logger.error('Getting usage log error', e);
   }
 
   return <UsageLog route={ApplicationRoute.UsageLog} />;
