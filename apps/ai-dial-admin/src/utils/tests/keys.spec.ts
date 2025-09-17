4232a55cd2bd7b0d6f151a8432780ecfbe174d45
Polina <palina_kryvasheyeva@epam.com>
2025-09-17 16:08:01 +0200

    chore: remove testId and add tests

--- apps/ai-dial-admin/src/utils/tests/keys.spec.ts
+++ apps/ai-dial-admin/src/utils/tests/keys.spec.ts
@@ -85,37 +85,43 @@ describe('Utils :: date :: calculateExpirationDate', () => {
 
   test('returns timestamp + 1 day for DAY period', () => {
     const result = calculateExpirationDate(ValidityPeriods.DAY);
-    const expected = Date.UTC(2025, 0, 2);
-    expect(result).toBe(expected);
+    const expected = new Date();
+    expected.setDate(expected.getDate() + 1);
+    expect(result).toBe(expected.toISOString());
   });
 
   test('returns timestamp + 7 days for WEEK period', () => {
     const result = calculateExpirationDate(ValidityPeriods.WEEK);
-    const expected = Date.UTC(2025, 0, 8);
-    expect(result).toBe(expected);
+    const expected = new Date();
+    expected.setDate(expected.getDate() + 7);
+    expect(result).toBe(expected.toISOString());
   });
 
   test('returns timestamp + 1 month for MONTH period', () => {
     const result = calculateExpirationDate(ValidityPeriods.MONTH);
-    const expected = Date.UTC(2025, 1, 1);
-    expect(result).toBe(expected);
+    const expected = new Date();
+    expected.setMonth(expected.getMonth() + 1);
+    expect(result).toBe(expected.toISOString());
   });
 
   test('returns timestamp + 3 months for THREE_MONTHS period', () => {
     const result = calculateExpirationDate(ValidityPeriods.THREE_MONTHS);
-    const expected = Date.UTC(2025, 3, 1);
-    expect(result).toBe(expected);
+    const expected = new Date();
+    expected.setMonth(expected.getMonth() + 3);
+    expect(result).toBe(expected.toISOString());
   });
 
   test('returns timestamp + 6 months for SIX_MONTHS period', () => {
     const result = calculateExpirationDate(ValidityPeriods.SIX_MONTHS);
-    const expected = Date.UTC(2025, 6, 1);
-    expect(result).toBe(expected);
+    const expected = new Date();
+    expected.setMonth(expected.getMonth() + 6);
+    expect(result).toBe(expected.toISOString());
   });
 
   test('returns timestamp + 1 year for YEAR period', () => {
     const result = calculateExpirationDate(ValidityPeriods.YEAR);
-    const expected = Date.UTC(2026, 0, 1);
-    expect(result).toBe(expected);
+    const expected = new Date();
+    expected.setFullYear(expected.getFullYear() + 1);
+    expect(result).toBe(expected.toISOString());
   });
 });
