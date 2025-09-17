f0fe702bf15a6c9c447aaf9aac1c2c731815b978
Polina <palina_kryvasheyeva@epam.com>
2025-09-17 22:45:27 +0200

    feature: [Entities] Add json editor for Core format

--- apps/ai-dial-admin/src/components/ApplicationRunners/ConfigurationView/Parameters.tsx
+++ apps/ai-dial-admin/src/components/ApplicationRunners/ConfigurationView/Parameters.tsx
@@ -26,7 +26,7 @@ const SchemeParameters: FC<Props> = ({ scheme, onChangeScheme }) => {
       <div className="flex flex-col flex-1 min-h-0 mt-3 w-full">
         <h1 className="mb-3">{t(EntityFieldsI18nKey.scheme)}</h1>
 
-        <EntityJsonEditor key={0} entity={editorScheme} errorNotifications={[]} setSelectedEntity={setEditorScheme} />
+        <EntityJsonEditor key={0} entity={editorScheme} setSelectedEntity={setEditorScheme} />
       </div>
     </div>
   );
