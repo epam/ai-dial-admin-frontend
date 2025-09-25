5938d76bb09abd67e13566fb9af92a39bb164dde
Polina <palina_kryvasheyeva@epam.com>
2025-09-25 18:29:30 +0200

    fix test

--- apps/ai-dial-admin/src/components/ActivityAudit/View/tests/utils.spec.ts
+++ apps/ai-dial-admin/src/components/ActivityAudit/View/tests/utils.spec.ts
@@ -16,7 +16,6 @@ import {
   isSimpleValueAddedOrRemoved,
   isSimpleValueChanged,
   mergeEntityMaps,
-  mergeLimits,
 } from '../utils';
 
 describe('Activity audit :: generateCurrentResource ', () => {
@@ -723,113 +722,5 @@ describe('Activity audit :: mergeEntityMaps', () => {
     const output = result.get(ActivityAuditResourceType.MODEL)!;
 
     expect(output).toEqual([]);
-  });
-});
-
-describe('Activity audit ::  mergeLimits', () => {
-  test('should merge two arrays with the same parameter', () => {
-    const arr1 = [{ parameter: 'myrole', value: 'minute: 1, day: 2, week: 3, month: 44' }];
-
-    const arr2 = [{ parameter: 'myrole', value: 'maxAcceptedUsers: 9, invitationTtl: 145', status: 'changed' }];
-
-    const expected = [
-      {
-        parameter: 'myrole',
-        value: 'minute: 1, day: 2, week: 3, month: 44, maxAcceptedUsers: 9, invitationTtl: 145',
-        status: 'changed',
-      },
-    ];
-
-    expect(mergeLimits(arr1, arr2)).toEqual(expected);
-  });
-
-  test('should merge arrays with different parameters', () => {
-    const arr1 = [{ parameter: 'myrole', value: 'minute: 1, day: 2, week: 3, month: 44' }];
-
-    const arr2 = [{ parameter: 'user', value: 'maxAcceptedUsers: 9, invitationTtl: 145', status: 'changed' }];
-
-    const expected = [
-      {
-        parameter: 'myrole',
-        value: 'minute: 1, day: 2, week: 3, month: 44',
-      },
-      {
-        parameter: 'user',
-        value: 'maxAcceptedUsers: 9, invitationTtl: 145',
-        status: 'changed',
-      },
-    ];
-
-    expect(mergeLimits(arr1, arr2)).toEqual(expected);
-  });
-
-  test('should handle missing status and default it to "unchanged"', () => {
-    const arr1 = [{ parameter: 'myrole', value: 'minute: 1, day: 2' }];
-
-    const arr2 = [{ parameter: 'myrole', value: 'week: 3, month: 44' }];
-
-    const expected = [
-      {
-        parameter: 'myrole',
-        value: 'minute: 1, day: 2, week: 3, month: 44',
-      },
-    ];
-
-    expect(mergeLimits(arr1, arr2)).toEqual(expected);
-  });
-
-  test('should update status to "changed" if present in second array', () => {
-    const arr1 = [{ parameter: 'myrole', value: 'minute: 1' }];
-
-    const arr2 = [{ parameter: 'myrole', value: 'day: 2, week: 3', status: 'changed' }];
-
-    const expected = [
-      {
-        parameter: 'myrole',
-        value: 'minute: 1, day: 2, week: 3',
-        status: 'changed',
-      },
-    ];
-
-    expect(mergeLimits(arr1, arr2)).toEqual(expected);
-  });
-
-  test('should handle empty arrays', () => {
-    const arr1 = [];
-    const arr2 = [];
-
-    const expected = [];
-
-    expect(mergeLimits(arr1, arr2)).toEqual(expected);
-  });
-
-  test('should return an array with only the first array content if the second is empty', () => {
-    const arr1 = [{ parameter: 'myrole', value: 'minute: 1' }];
-
-    const arr2 = [];
-
-    const expected = [
-      {
-        parameter: 'myrole',
-        value: 'minute: 1',
-      },
-    ];
-
-    expect(mergeLimits(arr1, arr2)).toEqual(expected);
-  });
-
-  test('should return an array with only the second array content if the first is empty', () => {
-    const arr1 = [];
-    const arr2 = [{ parameter: 'myrole', value: 'minute: 1', status: 'changed' }];
-
-    const expected = [
-      {
-        parameter: 'myrole',
-        value: 'minute: 1',
-        status: 'changed',
-      },
-    ];
-
-    expect(mergeLimits(arr1, arr2)).toEqual(expected);
   });
 });
