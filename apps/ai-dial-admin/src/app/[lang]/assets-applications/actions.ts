e001aa8a16fa0c2e3988a7ab8a732902e9f15099
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2025-09-25 13:29:38 +0300

    feature: [Assets] Application Details View (Issue #730) (#738)

--- apps/ai-dial-admin/src/app/[lang]/assets-applications/actions.ts
+++ apps/ai-dial-admin/src/app/[lang]/assets-applications/actions.ts
@@ -6,10 +6,24 @@ import { assetsApi } from '@/src/app/api/api';
 import { getUserToken } from '@/src/utils/auth/auth-request';
 import { getIsEnableAuthToggle } from '@/src/utils/env/get-auth-toggle';
 import { ResourceType } from '@/src/types/folder';
+import { DialAssetApp } from '@/src/models/dial/asset-app';
 
 export async function getApps(path: string) {
   const token = await getUserToken(getIsEnableAuthToggle(), headers(), cookies());
   return assetsApi.getAssetList(token, path, ResourceType.APPLICATION);
+}
+
+export async function getApp(folderId: string, name: string, version: string) {
+  const token = await getUserToken(getIsEnableAuthToggle(), headers(), cookies());
+  const apps = await assetsApi.getAssetList(token, `${folderId}/`, ResourceType.APPLICATION);
+  const path = apps?.find((app) => app.name === name && (app as DialAssetApp).version === version)?.path as string;
+
+  return assetsApi.getAsset(token, path, ResourceType.APPLICATION);
+}
+
+export async function updateApp(app: DialAssetApp) {
+  const token = await getUserToken(getIsEnableAuthToggle(), headers(), cookies());
+  return assetsApi.updateAsset(token, app, ResourceType.APPLICATION);
 }
 
 export async function removeApp(path: string) {
