12fcd3d990befc224f1b0e8b9ba5cb19889f0021
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2025-09-29 12:53:45 +0300

    feature: [Assets] Folder actions (#757)

--- apps/ai-dial-admin/src/utils/is-asset-view.ts
+++ apps/ai-dial-admin/src/utils/is-asset-view.ts
@@ -1,6 +1,6 @@
 import { ApplicationRoute } from '@/src/types/routes';
 
-export const isAssetView = (view: ApplicationRoute): boolean => {
+export const isAssetView = (view?: ApplicationRoute): boolean => {
   return (
     view === ApplicationRoute.Prompts || view === ApplicationRoute.Files || view === ApplicationRoute.AssetsApplications
   );
