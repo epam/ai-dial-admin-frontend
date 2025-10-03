151578295fabe2d871ac141603632a1ea988f7c8
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2025-10-03 13:00:13 +0300

    feature: [Interceptor Template] Create Interceptor (Issue #553) (#796)

--- apps/ai-dial-admin/src/components/InterceptorTemplates/View/View.tsx
+++ apps/ai-dial-admin/src/components/InterceptorTemplates/View/View.tsx
@@ -1,21 +1,31 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
 
+import { IconPlus } from '@tabler/icons-react';
 import { cloneDeep } from 'lodash';
 
 import { deleteInterceptorTemplate, updateInterceptorTemplate } from '@/src/app/[lang]/interceptor-templates/actions';
+import { createInterceptor } from '@/src/app/[lang]/interceptors/actions';
+import Button from '@/src/components/Common/Button/Button';
 import Tabs from '@/src/components/Common/Tabs/Tabs';
+import CreateEntity from '@/src/components/EntityListView/CreateEntity/CreateEntity';
+import { createModalTitleMap } from '@/src/components/EntityListView/constants';
 import EntityAudit from '@/src/components/EntityView/Audit/EntityAudit';
 import EntityHeader from '@/src/components/EntityView/Header/Header';
 import HeaderButtons from '@/src/components/EntityView/Header/HeaderButtons';
 import { auditTabs, EntityViewTab, interceptorsTabs, propertiesTabs } from '@/src/components/EntityView/View/utils';
 import ExtendedProperties from '@/src/components/InterceptorTemplates/Properties/ExtendedProperties';
 import Interceptors from '@/src/components/InterceptorTemplates/View/Interceptors/Interceptors';
+import { SOURCE_TYPE } from '@/src/components/SourceField/types';
+import { ButtonsI18nKey, DeleteI18nKey } from '@/src/constants/i18n';
+import { BASE_ICON_PROPS } from '@/src/constants/main-layout';
 import { useNotification } from '@/src/context/NotificationContext';
 import { useI18n } from '@/src/locales/client';
 import { InterceptorTemplate } from '@/src/models/interceptor-template';
+import { PopUpState } from '@/src/types/pop-up';
 import { ApplicationRoute } from '@/src/types/routes';
 import { isEqualSkippingUndefined } from '@/src/utils/is-equals-entity';
 import { getErrorNotification } from '@/src/utils/notification';
@@ -23,9 +33,10 @@ import { getErrorNotification } from '@/src/utils/notification';
 interface Props {
   route: ApplicationRoute;
   template: InterceptorTemplate;
+  names: string[];
 }
 
-const View: FC<Props> = ({ route, template }) => {
+const View: FC<Props> = ({ route, template, names }) => {
   const t = useI18n() as (stringToTranslate: string) => string;
   const router = useRouter();
   const { showNotification } = useNotification();
@@ -33,8 +44,19 @@ const View: FC<Props> = ({ route, template }) => {
   const [activeTab, setActiveTab] = useState(EntityViewTab.Properties);
   const [isChanged, setIsChanged] = useState(false);
   const [selectedTemplate, setSelectedTemplate] = useState(cloneDeep(template));
+  const [createModalState, setCreateModalState] = useState(PopUpState.Closed);
 
   const tabs = [propertiesTabs(t), interceptorsTabs(t), auditTabs(t)];
+
+  // todo change when source field will be added to create interceptor template modal
+  const source = useMemo(() => {
+    return {
+      source: {
+        $type: SOURCE_TYPE.RUNNER,
+        runnerName: selectedTemplate.name,
+      },
+    };
+  }, [selectedTemplate.name]);
 
   const onChangeActiveTab = useCallback(
     (tab: string) => {
@@ -80,7 +102,15 @@ const View: FC<Props> = ({ route, template }) => {
           removeEntity={deleteInterceptorTemplate}
           hideJsonEditor={true}
           jsonEditorEnabled={false}
-        />
+          childrenContainerClass={'flex-row-reverse'}
+        >
+          <Button
+            cssClass={`secondary`}
+            title={`${t(ButtonsI18nKey.Create)} ${t(DeleteI18nKey.Interceptor).toLowerCase()}`}
+            iconBefore={<IconPlus {...BASE_ICON_PROPS} />}
+            onClick={() => setCreateModalState(PopUpState.Opened)}
+          />
+        </HeaderButtons>
       </div>
       <div className="flex-1 overflow-auto mt-3 min-h-0">
         {activeTab === EntityViewTab.Properties && (
@@ -95,6 +125,19 @@ const View: FC<Props> = ({ route, template }) => {
         {activeTab === EntityViewTab.Audit && (
           <EntityAudit entity={selectedTemplate} view={ApplicationRoute.InterceptorTemplates} />
         )}
+        {createModalState === PopUpState.Opened &&
+          createPortal(
+            <CreateEntity
+              route={ApplicationRoute.Interceptors}
+              modalTitle={t(createModalTitleMap[ApplicationRoute.Interceptors])}
+              modalState={createModalState}
+              createEntity={createInterceptor}
+              onClose={() => setCreateModalState(PopUpState.Closed)}
+              names={names || []}
+              initialValues={source}
+            />,
+            document.body,
+          )}
       </div>
     </div>
   );
