8e12d58d5b2baa9535849117666bf6dea64f3f48
Polina <palina_kryvasheyeva@epam.com>
2025-10-07 16:05:09 +0200

    chore: use UI Kit components (Issue #825)

--- apps/ai-dial-admin/src/components/Common/Field/Field.tsx
+++ apps/ai-dial-admin/src/components/Common/Field/Field.tsx
@@ -5,14 +5,20 @@ import { useI18n } from '@/src/locales/client';
 import { BasicI18nKey } from '@/src/constants/i18n';
 
 interface Props {
-  htmlFor: string;
+  htmlFor?: string;
   fieldTitle?: string;
   optional?: boolean;
 }
 
-const Field: FC<Props> = ({ ...props }) => {
+const Field: FC<Props> = ({ fieldTitle, htmlFor, optional }) => {
   const t = useI18n();
-  return <DialFieldLabel optionalText={t(BasicI18nKey.Optional)} {...props} />;
+  return (
+    <DialFieldLabel
+      optionalText={t(BasicI18nKey.Optional)}
+      fieldTitle={fieldTitle}
+      optional={optional}
+      htmlFor={htmlFor || fieldTitle}
+  );
 };
 
 export default Field;
