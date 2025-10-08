fad061669c31cbb2bf6eb5954d1149bac8f57e7a
Valery Dluski <valerydluski@gmail.com>
2025-10-08 12:14:55 +0200

    fix: Update import path for @epam/ai-dial-ui-kit styles to use the correct distribution file

--- apps/ai-dial-admin/src/app/[lang]/layout.tsx
+++ apps/ai-dial-admin/src/app/[lang]/layout.tsx
@@ -21,7 +21,7 @@ import { getIsEnableAuthToggle } from '@/src/utils/env/get-auth-toggle';
 import { getMenuItems } from '@/src/utils/env/get-menu-items';
 import { PromptFolderProvider } from '@/src/context/assets/PromptFolderContext';
 
-import '@epam/ai-dial-ui-kit/styles.css';
+import '@epam/ai-dial-ui-kit/dist/styles.css';
 
 export default async function Layout({ children, params }: { children: ReactNode; params: Promise<{ lang: string }> }) {
   const { lang } = await params;
