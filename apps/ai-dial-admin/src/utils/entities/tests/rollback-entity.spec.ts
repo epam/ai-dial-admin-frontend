243f3f3fe09d5f78c3d91c3a03ffc29f30cf19c7
Polina <palina_kryvasheyeva@epam.com>
2025-10-15 19:14:30 +0200

    fix test

--- apps/ai-dial-admin/src/utils/entities/tests/rollback-entity.spec.ts
+++ apps/ai-dial-admin/src/utils/entities/tests/rollback-entity.spec.ts
@@ -1,4 +1,4 @@
-import { UpdateI18nKey } from '@/src/constants/i18n';
+import { RollbackI18nKey } from '@/src/constants/i18n';
 import { ActivityAuditResourceType } from '@/src/types/activity-audit';
 import { describe, expect, it } from 'vitest';
 import {
@@ -13,25 +13,25 @@ describe('Rollback :: utils', () => {
 
   it('getRollbackSuccessTitle returns a string', () => {
     expect(getRollbackSuccessTitle(ActivityAuditResourceType.INTERCEPTOR, tWithProps)).toBe(
-      `${UpdateI18nKey.NotificationTitle} with props`,
+      `${RollbackI18nKey.NotificationTitle} with props`,
     );
   });
 
   it('getRollbackSuccessDescription returns a string', () => {
     expect(getRollbackSuccessDescription(ActivityAuditResourceType.INTERCEPTOR, tWithProps)).toBe(
-      `${UpdateI18nKey.NotificationDescription} with props`,
+      `${RollbackI18nKey.NotificationDescription} with props`,
     );
   });
 
   it('getRollbackErrorTitle returns a string', () => {
     expect(getRollbackErrorTitle(ActivityAuditResourceType.INTERCEPTOR, tWithProps)).toBe(
-      `${UpdateI18nKey.NotificationTitle} with props`,
+      `${RollbackI18nKey.NotificationTitle} with props`,
     );
   });
 
   it('getRollbackErrorDescription returns a string', () => {
     expect(getRollbackErrorDescription(ActivityAuditResourceType.INTERCEPTOR, tWithProps)).toBe(
-      `${UpdateI18nKey.NotificationDescription} with props`,
+      `${RollbackI18nKey.NotificationDescription} with props`,
     );
   });
 });
