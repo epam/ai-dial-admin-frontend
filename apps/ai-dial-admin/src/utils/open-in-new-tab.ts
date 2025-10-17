8c2ff995d4f7dba9169b888e310da5181e321996
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-10-18 01:20:01 +0200

    fix: asset creation (#950)

--- apps/ai-dial-admin/src/utils/open-in-new-tab.ts
+++ apps/ai-dial-admin/src/utils/open-in-new-tab.ts
@@ -30,10 +30,14 @@ export const getEntityPath = (
     case ApplicationRoute.Prompts:
     case ApplicationRoute.Files:
     case ApplicationRoute.AssetsApplications:
-    case ApplicationRoute.AssetsToolsets:
+    case ApplicationRoute.AssetsToolsets: {
+      const path =
+        (data as DialPrompt).path ||
+        `${(data as DialPrompt).folderId}${(data as DialPrompt).name}__${(data as DialPrompt).version}`;
       return forRemove
         ? decodeURIComponent((data as DialPrompt).path)
-        : `${encodeURIComponent((data as DialPrompt).name as string)}?path=${encodeURIComponent((data as DialPrompt).path || (data as DialPrompt).folderId)}`;
+        : `${encodeURIComponent((data as DialPrompt).name as string)}?path=${encodeURIComponent(path)}`;
+    }
 
     case ApplicationRoute.PromptPublications:
     case ApplicationRoute.FilePublications:
