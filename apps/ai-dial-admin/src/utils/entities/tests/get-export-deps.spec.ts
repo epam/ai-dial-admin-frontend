a9e10381592d64d38e5d8d59b2fe87bcf0581b68
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2025-10-21 14:17:41 +0300

    hotfix: core dependencies (#977)
    Co-authored-by: Polina <palina_kryvasheyeva@epam.com>

---
+++ apps/ai-dial-admin/src/utils/entities/tests/get-export-deps.spec.ts
@@ -0,0 +1,53 @@
+import { EntityType } from '@/src/types/entity-type';
+import { describe, expect, test } from 'vitest';
+import { getAllAvailableDependencies } from '../get-export-deps';
+
+describe('Export Config Utils :: getAllAvailableDependencies', () => {
+  test('returns correct dependencies for ROLE', () => {
+    const result = getAllAvailableDependencies(EntityType.ROLE);
+    expect(result).toEqual([
+      EntityType.MODEL,
+      EntityType.APPLICATION,
+      EntityType.TOOLSET,
+      EntityType.ROUTE,
+      EntityType.APPLICATION_TYPE_SCHEMA,
+      EntityType.INTERCEPTOR,
+    ]);
+  });
+
+  test('returns correct dependencies for KEY', () => {
+    const result = getAllAvailableDependencies(EntityType.KEY);
+    expect(result).toEqual([
+      EntityType.ROLE,
+      EntityType.MODEL,
+      EntityType.APPLICATION,
+      EntityType.APPLICATION_TYPE_SCHEMA,
+      EntityType.INTERCEPTOR,
+    ]);
+  });
+
+  test('returns correct dependencies for MODEL', () => {
+    const result = getAllAvailableDependencies(EntityType.MODEL);
+    expect(result).toEqual([EntityType.INTERCEPTOR]);
+  });
+
+  test('returns correct dependencies for APPLICATION', () => {
+    const result = getAllAvailableDependencies(EntityType.APPLICATION);
+    expect(result).toEqual([
+      EntityType.MODEL,
+      EntityType.APPLICATION,
+      EntityType.APPLICATION_TYPE_SCHEMA,
+      EntityType.INTERCEPTOR,
+    ]);
+  });
+
+  test('returns empty array for undefined input', () => {
+    const result = getAllAvailableDependencies(undefined);
+    expect(result).toEqual([]);
+  });
+
+  test('returns empty array for unsupported type', () => {
+    const result = getAllAvailableDependencies('UNKNOWN' as EntityType);
+    expect(result).toEqual([]);
+  });
+});
