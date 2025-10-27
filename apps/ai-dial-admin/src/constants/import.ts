76c1ddaefcf77c2d98c6feb1c03601f28c4b6f78
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-10-27 10:48:40 +0100

    chore: replace Steps to DialSteps (Issue #825) (#1026)

--- apps/ai-dial-admin/src/constants/import.ts
+++ apps/ai-dial-admin/src/constants/import.ts
@@ -1,8 +1,7 @@
-import { RadioButtonWithContent } from '@epam/ai-dial-ui-kit';
+import { RadioButtonWithContent, Step } from '@epam/ai-dial-ui-kit';
 
 import { ImportI18nKey, TabsI18nKey } from '@/src/constants/i18n';
 import { ConflictResolutionPolicy, ImportFileType, ImportSteps } from '@/src/types/import';
-import { Step, StepStatus } from '@/src/models/step';
 import { ApplicationRoute } from '@/src/types/routes';
 
 export const IMPORT_RESOLUTIONS = (
@@ -20,13 +19,13 @@ export const IMPORT_RESOLUTIONS = (
 };
 
 export const IMPORT_STEPS = (t: (stringToTranslate: string) => string): Step[] => [
-  { id: ImportSteps.FILES, name: t(ImportI18nKey.Files), status: StepStatus.INVALID },
-  { id: ImportSteps.PROPERTIES, name: t(TabsI18nKey.Properties), status: StepStatus.INVALID },
+  { id: ImportSteps.FILES, name: t(ImportI18nKey.Files) },
+  { id: ImportSteps.PROPERTIES, name: t(TabsI18nKey.Properties) },
 ];
 
 export const IMPORT_CONFIG_STEPS = (t: (stringToTranslate: string) => string): Step[] => [
-  { id: ImportSteps.FILES, name: t(ImportI18nKey.Files), status: StepStatus.INVALID },
-  { id: ImportSteps.CONFIGURATION, name: t(ImportI18nKey.Configuration), status: StepStatus.INVALID },
+  { id: ImportSteps.FILES, name: t(ImportI18nKey.Files) },
+  { id: ImportSteps.CONFIGURATION, name: t(ImportI18nKey.Configuration) },
 ];
 
 export const IMPORT_FILE_TYPES = (
