56c9138684d8124b89aab9a203f641c48dd5fd2d
Polina <palina_kryvasheyeva@epam.com>
2025-10-28 12:17:16 +0100

    feature: [Toolset/Toolset publications] Add auth settings

--- apps/ai-dial-admin/src/components/Assets/Toolsets/View.tsx
+++ apps/ai-dial-admin/src/components/Assets/Toolsets/View.tsx
@@ -187,7 +187,6 @@ const ToolsetView: FC<Props> = ({ etag, originalToolset, toolsets }) => {
 
   return (
     <>
-      {' '}
       <div className="flex flex-col flex-1 min-h-0 w-full bg-layer-2 rounded p-4 pb-14 lg:pb-4 relative">
         <div className={headerClassName}>
           {!jsonEditorEnabled && (
