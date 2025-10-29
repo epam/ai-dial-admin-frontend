3eb2c54bf6817d94b0136186667027708ce2c711
Polina <palina_kryvasheyeva@epam.com>
2025-10-29 16:22:01 +0100

    fix: [App Routes] Missing “Response attachment paths” label until a field is added

--- apps/ai-dial-admin/src/components/EntityView/AppRoute/CreateRoute.tsx
+++ apps/ai-dial-admin/src/components/EntityView/AppRoute/CreateRoute.tsx
@@ -1,8 +1,10 @@
 import { DialFormPopup, DialTextInputField, PopupSize } from '@epam/ai-dial-ui-kit';
 import { FC, useState } from 'react';
 
-import { ButtonsI18nKey, CreateI18nKey, EntityFieldsI18nKey, EntityPlaceholdersI18nKey } from '@/src/constants/i18n';
+import { getCreateEntityTitle } from '@/src/utils/entities/create-entity';
+import { ButtonsI18nKey, EntityFieldsI18nKey, EntityPlaceholdersI18nKey } from '@/src/constants/i18n';
 import { useI18n } from '@/src/locales/client';
+import { ApplicationRoute } from '@/src/types/routes';
 
 interface Props {
   isModalOpen: boolean;
@@ -18,7 +20,7 @@ const CreateRoute: FC<Props> = ({ isModalOpen, onClose, onCreate }) => {
   return (
     <DialFormPopup
       onClose={onClose}
-      title={t(CreateI18nKey.Route)}
+      title={getCreateEntityTitle(ApplicationRoute.Routes, t)}
       submitLabel={t(ButtonsI18nKey.Create)}
       size={PopupSize.Sm}
       onSubmit={() => {
