52862cfda1b86325bee5a7102421e4bd4e5aef8b
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-10-30 16:16:47 +0100

    fix: tests

--- apps/ai-dial-admin/src/components/EntityListView/Components/tests/utils.spec.ts
+++ apps/ai-dial-admin/src/components/EntityListView/Components/tests/utils.spec.ts
@@ -11,8 +11,29 @@ describe('Utils :: prepareEntityForDuplicate', () => {
     roles: ['roles'],
   };
 
-  test('Should return filtered interceptor', () => {
-    const result = prepareEntityForDuplicate(ApplicationRoute.Interceptors, entity);
+  test('Should return filtered role', async () => {
+    const result = await prepareEntityForDuplicate(
+      ApplicationRoute.Roles,
+      {
+        name: 'n',
+        description: 'd',
+        grantedKeys: ['key'],
+        share: { a: {} },
+        limits: { a: {} },
+      },
+      {} as any,
+    );
+    expect(result).toEqual({
+      name: 'n',
+      description: 'd',
+      grantedKeys: [],
+      limits: {},
+      share: {},
+    });
+  });
+
+  test('Should return filtered interceptor', async () => {
+    const result = await prepareEntityForDuplicate(ApplicationRoute.Interceptors, entity, {} as any);
     expect(result).toEqual({
       name: 'n',
       description: 'd',
@@ -22,8 +43,8 @@ describe('Utils :: prepareEntityForDuplicate', () => {
     });
   });
 
-  test('Should return filtered keys', () => {
-    const result = prepareEntityForDuplicate(ApplicationRoute.Keys, entity);
+  test('Should return filtered keys', async () => {
+    const result = await prepareEntityForDuplicate(ApplicationRoute.Keys, entity, {} as any);
     expect(result).toEqual({
       name: 'n',
       description: 'd',
@@ -33,19 +54,62 @@ describe('Utils :: prepareEntityForDuplicate', () => {
     });
   });
 
-  test('Should return original entity', () => {
-    const result = prepareEntityForDuplicate(ApplicationRoute.Models, entity);
+  test('Should return original entity', async () => {
+    const result = await prepareEntityForDuplicate(ApplicationRoute.Models, entity, {} as any);
     expect(result).toEqual(entity);
   });
 
-  test('Should return original entity', () => {
-    const result1 = prepareEntityForDuplicate(ApplicationRoute.Prompts, entity);
-    expect(result1).toEqual({ ...entity, description: void 0, content: void 0 });
-
-    const result2 = prepareEntityForDuplicate(ApplicationRoute.Prompts, entity, {
-      description: 'description',
-      content: 'content',
+  test('Should return original entity for Prompts', async () => {
+    const result1 = await prepareEntityForDuplicate(ApplicationRoute.Prompts, { ...entity, folderId: 'folder' }, {
+      current: {
+        folderId: 'aaa',
+        name: 'prompt',
+        version: '1.0.0',
+      },
     } as any);
-    expect(result2).toEqual({ ...entity, description: 'description', content: 'content' });
+    expect(result1).toEqual({
+      ...entity,
+      description: void 0,
+      content: void 0,
+      folderId: 'folder',
+    });
+  });
+
+  test('Should return original entity for Apps', async () => {
+    const result1 = await prepareEntityForDuplicate(
+      ApplicationRoute.AssetsApplications,
+      { ...entity, folderId: 'folder' },
+      {
+        current: {
+          folderId: 'aaa',
+          name: 'app',
+          version: '1.0.0',
+        },
+      } as any,
+    );
+    expect(result1).toEqual({
+      ...entity,
+      description: 'd',
+      folderId: 'folder',
+    });
+  });
+
+  test('Should return original entity for Toolsets', async () => {
+    const result1 = await prepareEntityForDuplicate(
+      ApplicationRoute.AssetsToolsets,
+      { ...entity, folderId: 'folder' },
+      {
+        current: {
+          folderId: 'aaa',
+          name: 'toolset',
+          version: '1.0.0',
+        },
+      } as any,
+    );
+    expect(result1).toEqual({
+      ...entity,
+      description: 'd',
+      folderId: 'folder',
+    });
   });
 });
