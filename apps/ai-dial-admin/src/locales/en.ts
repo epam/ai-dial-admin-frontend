8ae1cd941c9e43e47e48a06a64a35a979df47f34
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2025-11-05 14:53:46 +0300

    fix: [Approvals] Decline popup doesn’t indicate minimum 15 characters requirement for comment (Issue #1132) (#1141)

--- apps/ai-dial-admin/src/locales/en.ts
+++ apps/ai-dial-admin/src/locales/en.ts
@@ -951,6 +951,7 @@ export default {
     DisplayNameErrorModel:
       'This name is used by versionless model. Specify version for model with this display name to group models.',
     EditorErrorLine: 'The error line is {line}',
+    CommentError: 'Comment must be at least 15 characters long',
   },
   Source: {
     OpenTemplate: 'Open template',
