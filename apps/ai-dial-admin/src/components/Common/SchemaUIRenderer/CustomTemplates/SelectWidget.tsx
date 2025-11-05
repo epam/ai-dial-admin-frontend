2df37313bdedb5bbcb3d46721a695f1a779a22e5
Yauheni Osipau <yauheni_osipau@epam.com>
2025-11-05 16:41:43 +0300

    feat: added toggler for props

--- apps/ai-dial-admin/src/components/Common/SchemaUIRenderer/CustomTemplates/SelectWidget.tsx
+++ apps/ai-dial-admin/src/components/Common/SchemaUIRenderer/CustomTemplates/SelectWidget.tsx
@@ -3,9 +3,17 @@ import { FC, useCallback } from 'react';
 import { DialSelect } from '@epam/ai-dial-ui-kit';
 import { WidgetProps } from '@rjsf/utils';
 
-import { WidgetHeader } from './WidgetHeader';
+import { WidgetHeader } from '@/src/components/Common/SchemaUIRenderer/Components/WidgetHeader';
 
-export const SelectWidget: FC<WidgetProps> = ({ options, value, multiple = false, onChange, label }) => {
+export const SelectWidget: FC<WidgetProps> = ({
+  options,
+  value,
+  multiple = false,
+  onChange,
+  label,
+  readonly,
+  schema,
+}) => {
   const { enumOptions } = options;
   const emptyValue = multiple ? [] : '';
 
@@ -21,8 +29,9 @@ export const SelectWidget: FC<WidgetProps> = ({ options, value, multiple = false
 
   return (
     <div className="flex flex-col w-full bg-layer-2 p-[18px]">
-      {label && <WidgetHeader title={label} defaultHeader={true} />}
+      {label && <WidgetHeader title={label} defaultHeader={true} description={schema.description} />}
       <DialSelect
+        disabled={readonly}
         options={enumOptions?.map((o) => ({ ...o, value: o.value.toString() })) || []}
         value={selectedIndexes === void 0 ? emptyValue : selectedIndexes}
         onChange={handleChange}
