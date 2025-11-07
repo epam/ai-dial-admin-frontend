c9c9cd1f4d2a8c530be608454ead15fdb3ca1b29
Polina <palina_kryvasheyeva@epam.com>
2025-11-07 13:48:08 +0100

    feature: Add CSP

--- apps/ai-dial-admin/next.config.js
+++ apps/ai-dial-admin/next.config.js
@@ -22,12 +22,11 @@ const ContentSecurityPolicy = `
     base-uri 'self';
     ${process.env.NODE_ENV === 'production' ? 'upgrade-insecure-requests;' : ''}
     frame-ancestors ${process.env.ALLOWED_FRAME_ANCESTORS ?? "'none'"};
+    script-src 'self' 'nonce-${nonce}' 'https: http: ${
+    process.env.NODE_ENV === 'production' ? '' : `'unsafe-eval'`
+    }';
 `;
 
-    // script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: ${
-    //   process.env.NODE_ENV === 'production' ? '' : `'unsafe-eval'`
-    // };
-    // frame-ancestors ${process.env.ALLOWED_FRAME_ANCESTORS ?? "'none'"};
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
