7d17fdddaa75f0971b4d0bad0ccffd486959caf8
Yauheni Osipau <yauheni_osipau@epam.com>
2025-11-11 14:20:55 +0300

    fix: remove validation

--- apps/ai-dial-admin/src/components/Applications/ParametersTab/ParametersTab.tsx
+++ apps/ai-dial-admin/src/components/Applications/ParametersTab/ParametersTab.tsx
@@ -126,7 +126,7 @@ const ApplicationParametersTab: FC<Props> = ({
         applicationPropertiesTemp: props,
       } as unknown as BaseEntity;
       onChangeEntity?.(newEntity, isSkipRefresh);
-      const isValid = !props?.some((p) => !p.key || p.value == null || p.value === '');
+      const isValid = !props?.some((p) => !p.key || p.value === void 0 || p.value === '');
       dispatch({ type: ValidationActionType.SetField, field: 'applicationProperties', isValid });
     },
     [dispatch, entity, onChangeEntity],
