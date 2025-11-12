7e1a03901364eb91c1badae6ab4eb4196758eddf
Yauheni Osipau <yauheni_osipau@epam.com>
2025-11-12 12:48:24 +0300

    fix build

--- apps/ai-dial-admin/src/components/AddEntitiesTab/tests/utils.spec.ts
+++ apps/ai-dial-admin/src/components/AddEntitiesTab/tests/utils.spec.ts
@@ -197,7 +197,10 @@ describe('Add Entities tab :: getRelevantAppRunnersForInterceptor ', () => {
 
   test('Should return array with model', () => {
     expect(
-      getRelevantAppRunnersForInterceptor({ applicationTypeSchemas: ['applicationRunner', 'applicationRunner1'] }, data),
+      getRelevantAppRunnersForInterceptor(
+        { applicationTypeSchemas: ['applicationRunner', 'applicationRunner1'] },
+        data,
+      ),
     ).toEqual([data[4]]);
   });
 });
