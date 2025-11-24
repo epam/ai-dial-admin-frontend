825af37f9d7452459fa4851bfd3937be376b24c6
Polina <palina_kryvasheyeva@epam.com>
2025-11-24 22:53:03 +0100

    fix test

--- apps/ai-dial-admin/src/utils/time-filter/get-time-range-id.ts
+++ apps/ai-dial-admin/src/utils/time-filter/get-time-range-id.ts
@@ -2,7 +2,7 @@ import { TimeRange } from '@/src/models/time-range';
 import { timePeriodOptionsConfig } from '@/src/constants/global-time-filter';
 
 export const getTimeRangeById = (periodId: string): TimeRange => {
-  const config = timePeriodOptionsConfig.find((item) => item.label === periodId);
+  const config = timePeriodOptionsConfig.find((item) => item.value === periodId);
   const now = new Date();
   const startDate = new Date(now.getTime() - (config?.offset ?? 0));
 
