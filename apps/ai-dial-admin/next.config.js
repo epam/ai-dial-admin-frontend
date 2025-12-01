7a11d22093a79d938fa74e9875b6605addb93885
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-12-01 13:25:56 +0100

    chore: disable partially CSP policy

--- apps/ai-dial-admin/next.config.js
+++ apps/ai-dial-admin/next.config.js
@@ -11,11 +11,11 @@ try {
 }
 
 console.log('ALLOWED_IFRAME_ORIGINS:', process.env.ALLOWED_IFRAME_ORIGINS);
+// script-src 'self' 'unsafe-inline' 'unsafe-eval' ${process.env.ALLOWED_IFRAME_ORIGINS || ''};
+// frame-src 'self' ${process.env.ALLOWED_IFRAME_ORIGINS || ''};  
 
 const ContentSecurityPolicy = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' ${process.env.ALLOWED_IFRAME_ORIGINS || ''};
-    frame-src 'self' ${process.env.ALLOWED_IFRAME_ORIGINS || ''};  
     style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline';
     img-src 'self' blob: data: https://authjs.dev ${process.env.ALLOWED_IMAGE_ORIGINS || ''};
     font-src 'self' data: https://cdn.jsdelivr.net fonts.gstatic.com;
