68df98f4da185e7d131c0d92a04790d42c5f59dd
Polina <palina_kryvasheyeva@epam.com>
2025-12-04 14:57:03 +0100

    feat: Import Applications

--- apps/ai-dial-admin/src/server/entities/assets/folders-api.ts
+++ apps/ai-dial-admin/src/server/entities/assets/folders-api.ts
@@ -7,6 +7,7 @@ import { ResourceType } from '@/src/types/resource-type';
 import { ApplicationRoute } from '@/src/types/routes';
 import { API } from '../../api';
 import { BaseApi } from '../../base-api';
+import { buildAssetUrl } from './utils';
 
 export const FOLDERS_URL = `${API}/folders`;
 export const FOLDERS_MOVE_URL = `${FOLDERS_URL}/move`;
