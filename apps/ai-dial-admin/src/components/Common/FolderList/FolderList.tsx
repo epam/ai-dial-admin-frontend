98969be3e8df9b69892563ab2be3567edf7c33f8
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-12-11 08:54:47 +0100

    feature: [Assets] Create Folder from Applications/Toolset Archive (#1430)

--- apps/ai-dial-admin/src/components/Common/FolderList/FolderList.tsx
+++ apps/ai-dial-admin/src/components/Common/FolderList/FolderList.tsx
@@ -234,9 +234,7 @@ const FolderList: FC<Props> = ({
 
                 {showFolderActions && (
                   <div className="invisible group-hover:visible text-primary mx-2 flex flex-row gap-2">
-                    {(view === ApplicationRoute.Prompts ||
-                      view === ApplicationRoute.AssetsApplications ||
-                      view === ApplicationRoute.Files) && (
+                    {isAssetView(view) && (
                       <ActionsDropdown items={folderCreateItems(node)} icon={<IconPlus {...BASE_ICON_PROPS} />} />
                     )}
                     <ActionsDropdown items={folderManageItems(node)} icon={<IconDotsVertical {...BASE_ICON_PROPS} />} />
