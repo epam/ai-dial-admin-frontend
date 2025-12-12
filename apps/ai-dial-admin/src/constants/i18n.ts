00a34ab6eec69b3830bfbcf0be21721b859d82d3
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-12-12 12:53:56 +0100

    fix: [Assets] Extra sentence in confirmation pop-up (Issue #1453) (#1457)

--- apps/ai-dial-admin/src/constants/i18n.ts
+++ apps/ai-dial-admin/src/constants/i18n.ts
@@ -283,6 +283,7 @@ export enum UpdateI18nKey {
   // Notification
   NotificationTitle = 'UpdateEntity.Notification.Title',
   NotificationDescription = 'UpdateEntity.Notification.Description',
+  NotificationDescriptionAssets = 'UpdateEntity.Notification.DescriptionAssets',
 
   // Entities
   Model = 'UpdateEntity.Entities.Model',
