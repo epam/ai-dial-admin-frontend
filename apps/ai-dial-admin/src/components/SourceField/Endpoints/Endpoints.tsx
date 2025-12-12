81878379d48d723449ad9a5c5d16b2fc8ee2ae26
Siarhei Simanovich <Siarhei_Simanovich@epam.com>
2025-12-12 16:04:47 +0100

    Update

--- apps/ai-dial-admin/src/components/SourceField/Endpoints/Endpoints.tsx
+++ apps/ai-dial-admin/src/components/SourceField/Endpoints/Endpoints.tsx
@@ -5,6 +5,7 @@ import ToolsetEndpoint from '@/src/components/SourceField/Endpoints/ToolsetEndpo
 import { Toolset } from '@/src/models/dial/toolset';
 import InterceptorEndpoint from '@/src/components/SourceField/Endpoints/InterceptorEndpoint';
 import { DialInterceptor } from '@/src/models/dial/interceptor';
+import { SOURCE_TYPE } from '../types';
 
 interface Props<T> {
   entity: T;
@@ -28,7 +29,7 @@ const Endpoints = <T extends object>({ entity, onChange, view, isModal, prefix }
       {view === ApplicationRoute.Toolsets && (
         <ToolsetEndpoint
           entity={entity as Toolset}
-          disabled={true}
+          disabled={(entity as Toolset).source?.$type === SOURCE_TYPE.CONTAINER}
           onChange={onChange as (entity: Toolset) => void}
           isModal={isModal}
           prefix={prefix}
