8a6f25ed4698974783a04fd99c3b5eec11ba31b0
Yauheni Osipau <yauheni_osipau@epam.com>
2025-12-19 14:48:00 +0300

    fix: support failed message for import/create folder

--- apps/ai-dial-admin/src/components/Common/FolderList/Modals/FolderListModals.tsx
+++ apps/ai-dial-admin/src/components/Common/FolderList/Modals/FolderListModals.tsx
@@ -6,15 +6,18 @@ import { changeFolder, createFolderWithFiles, removeFolder } from '@/src/app/[la
 import FilePathModal from '@/src/components/Common/FilePath/FilePathModal';
 import FolderCreateModal from '@/src/components/Common/FolderCreate/Components/FolderCreateModal';
 import { getResourceTypeByView } from '@/src/components/Common/FolderList/utils';
-import { getFormDataForImport } from '@/src/components/EntityListView/HeaderButtons/utils';
+import { getFormDataForImport, getImportTitle } from '@/src/components/EntityListView/HeaderButtons/utils';
+import { getImportResults } from '@/src/components/EntityListView/Import/utils';
 import { ROOT_FOLDER } from '@/src/constants/file';
 import { BasicI18nKey, FoldersI18nKey } from '@/src/constants/i18n';
 import { AssetsFolderContext } from '@/src/context/assets/AssetsFolderContext';
 import { useNotification } from '@/src/context/NotificationContext';
+import { useProtectedRequest } from '@/src/hooks/use-protected-request';
 import { useI18n } from '@/src/locales/client';
 import { DialFile } from '@/src/models/dial/file';
 import { DialFolder } from '@/src/models/dial/folder';
 import { DialRule } from '@/src/models/dial/rule';
+import { ImportResult } from '@/src/models/import';
 import { ImportData } from '@/src/models/import-asset';
 import { ConflictResolutionPolicy, ImportFileType } from '@/src/types/import';
 import { ApplicationRoute } from '@/src/types/routes';
@@ -23,7 +26,6 @@ import { getFolderNameAndPath } from '@/src/utils/files/path';
 import { getSuccessNotification } from '@/src/utils/notification';
 import DeleteFolder from './DeleteFolder';
 import RenameFolder from './RenameFolder';
-import { useProtectedRequest } from '@/src/hooks/use-protected-request';
 
 export enum ModalType {
   create = 'create',
@@ -62,7 +64,10 @@ const FolderListModals: FC<Props> = ({ isModalOpen, modalType, view, selectedFol
       getReqRef.current(createFolderWithFiles, body, fileType, view).then((res) => {
         if (res.success) {
           folderContext?.fetchFiles?.(`${ROOT_FOLDER}/`, true);
+          const results = (res.response as { importResults: ImportResult[] }).importResults;
+          const translatedType = t(getImportTitle(view)).toLowerCase();
           showNotification(getSuccessNotification(t(FoldersI18nKey.FolderCreateSuccess)));
+          getImportResults(results, getFolderName(path) as string, translatedType, t, showNotification);
         }
       });
       handleClose();
