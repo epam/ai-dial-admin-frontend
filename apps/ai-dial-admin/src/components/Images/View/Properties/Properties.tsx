45391e2b123dbc35b7fa878543913d466148464f
Siarhei Simanovich <Siarhei_Simanovich@epam.com>
2026-01-07 16:56:06 +0100

    Use common components

--- apps/ai-dial-admin/src/components/Images/View/Properties/Properties.tsx
+++ apps/ai-dial-admin/src/components/Images/View/Properties/Properties.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { DialLabelledText, DialTooltip } from '@epam/ai-dial-ui-kit';
+import { DialLabelledText } from '@epam/ai-dial-ui-kit';
 
 import { Image } from '@/src/models/deployments/images';
 import { EntitiesI18nKey, EntityFieldsI18nKey, ImagesI18nKey } from '@/src/constants/i18n';
@@ -9,7 +9,7 @@ import { useI18n } from '@/src/locales/client';
 
 import ImageFields from '@/src/components/Images/Fields/ImageFields';
 import StatusIndicator from '@/src/components/Deployments/Common/StatusIndicator/StatusIndicator';
-import CopyButton from '@/src/components/Common/CopyButton/CopyButton';
+import LabelledText from '@/src/components/Common/LabelledText/LabelledText';
 
 interface Props {
   image: Image;
@@ -25,14 +25,7 @@ const Properties: FC<Props> = ({ image, setImage, originalName }) => {
   return (
     <div className="flex flex-col pt-3 w-full divide-y divide-primary min-h-0 flex-1">
       <div className="flex gap-10 overflow-y-scroll">
-        <DialLabelledText label={t(EntityFieldsI18nKey.id)}>
-          <p className="flex items-center gap-2 max-w-[360px]">
-            <DialTooltip tooltip={originalName}>
-              <span className="truncate">{originalName}</span>
-            </DialTooltip>
-            <CopyButton field={originalName} label={t(EntityFieldsI18nKey.id)} />
-          </p>
-        </DialLabelledText>
+        <LabelledText label={t(EntityFieldsI18nKey.id)} text={originalName} tooltip={originalName} copyable />
         <DialLabelledText label={t(EntityFieldsI18nKey.type)} text={t(ImagesI18nKey.Image)} />
         <DialLabelledText
           label={t(EntityFieldsI18nKey.createdAt)}
