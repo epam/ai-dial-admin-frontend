fb1416e379360dc7391ab5573fb9d6371e8a281f
Yauheni Osipau <yauheni_osipau@epam.com>
2026-01-09 10:32:59 +0300

    added missing i18n

--- apps/ai-dial-admin/src/components/Common/SyncCoreStatus/DiffModal.tsx
+++ apps/ai-dial-admin/src/components/Common/SyncCoreStatus/DiffModal.tsx
@@ -71,7 +71,7 @@ const CoreSyncDiffModal: FC<Props> = ({
         ) : newStatus?.status === EntitySyncStatus.FULLY_SYNCED ? (
           <div className="flex flex-col gap-y-2 items-center justify-center h-[250px] pb-7">
             <IconCircleCheck stroke={1} size={60} className="text-secondary" />
-            <div className="text-sm">Fully synced</div>
+            <div className="text-sm">{t(CoreSyncI18nKey.Fully)}</div>
           </div>
         ) : (
           <div className="flex flex-col h-[80vh] flex-1">
