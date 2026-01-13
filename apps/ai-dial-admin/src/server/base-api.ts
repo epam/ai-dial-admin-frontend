d7affedecb1b9e9feeb61b77b3af3a34f3511b2f
Polina <palina_kryvasheyeva@epam.com>
2026-01-13 16:38:24 +0100

    feature: add trace and span id for pino logger

--- apps/ai-dial-admin/src/server/base-api.ts
+++ apps/ai-dial-admin/src/server/base-api.ts
@@ -3,7 +3,7 @@ import { JWT } from 'next-auth/jwt';
 import { ServerActionResponse } from '@/src/models/server-action';
 import { streamRequest } from '@/src/utils/api/create-stream-request';
 import { ErrorObject, getError, getErrorMessage, getParsedError } from '@/src/utils/api/error';
-import { logger } from './logger';
+import { logErrorText } from './logger';
 import { sendRequest } from '@/src/utils/api/send-request';
 import { getApiHeaders, getAuthorizationHeader } from '@/src/utils/auth/api-headers';
 import { fileRequest } from '@/src/utils/api/file-request';
@@ -118,16 +118,16 @@ export class BaseApi {
       (res) => {
         if (isFailedRequest(res)) {
           const traceparent = res.headers.get('traceparent');
-          logger.error(`Request status ${res.status}`);
-          logger.error(`Request error Url  ${res.url}`);
-          logger.error(`Request error traceparent  ${traceparent}`);
+          logErrorText(`Request status ${res.status}`);
+          logErrorText(`Request error Url  ${res.url}`);
+          logErrorText(`Request error traceparent  ${traceparent}`);
 
           if (res.status === 403) {
             return void 0;
           }
 
           return res.text().then((error) => {
-            logger.error(`Request error ${res.status} ${error}`);
+            logErrorText(`Request error ${res.status} ${error}`);
             return null;
           });
         }
@@ -177,14 +177,14 @@ export class BaseApi {
   }
 
   private setLoggerRequestInfoError(res: Response) {
-    logger.error(`Request status ${res.status}`);
-    logger.error(`Request error Url  ${res.url}`);
+    logErrorText(`Request status ${res.status}`);
+    logErrorText(`Request error Url  ${res.url}`);
   }
 
   private setLoggerRequestError(error: string, res: Response) {
     const errObject = getParsedError(error);
-    logger.error(`Request error ${res.status}`);
-    logger.error(`${errObject.error} ${errObject.message}`);
+    logErrorText(`Request error ${res.status}`);
+    logErrorText(`${errObject.error} ${errObject.message}`);
   }
 }
 
--- apps/ai-dial-admin/src/server/logger.ts
+++ apps/ai-dial-admin/src/server/logger.ts
@@ -1,5 +1,6 @@
 import pino from 'pino';
 import pretty from 'pino-pretty';
+import { context, trace } from '@opentelemetry/api';
 
 const stream = pretty({
   colorize: true,
@@ -7,16 +8,35 @@ const stream = pretty({
   translateTime: 'yyyy-mm-dd HH:MM:ss.l o',
 });
 
-export const logger = pino(stream);
+const logger = pino(stream);
+
+export const getCurrentTraceIds = () => {
+  const span = trace.getSpan(context.active());
+  return {
+    trace_id: span?.spanContext().traceId ?? 'no-trace',
+    span_id: span?.spanContext().spanId ?? 'no-span',
+  };
+};
+
+export const logWithTrace = (msg: string, level: 'info' | 'error' | 'warn' = 'info') => {
+  const traceIds = getCurrentTraceIds();
+  logger[level]({ ...traceIds }, msg);
+};
+
+export const logErrorText = (message: string, context: Record<string, unknown> = {}) => {
+  logError({}, message, context);
+};
 
 export const logError = (error: unknown, message: string, context: Record<string, unknown> = {}) => {
   const errorMessage = (error as { message: string }).message
     ? (error as { message: string }).message
     : 'Unknown error';
   const errorStack = (error as { error: string }) ? (error as { error: string }).error : 'No stack trace available';
+  const traceIds = getCurrentTraceIds();
 
   logger.error(
     {
+      ...traceIds,
       error: {
         message: errorMessage,
         stack: errorStack,
