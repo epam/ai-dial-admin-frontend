ebfe60017234b465f0b8b00824aaf0162b37f396
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-01-19 22:54:56 +0100

    feature: [Toolsets] Support auth

--- apps/ai-dial-admin/src/app/[lang]/toolsets/[id]/page.tsx
+++ apps/ai-dial-admin/src/app/[lang]/toolsets/[id]/page.tsx
@@ -13,10 +13,14 @@ import { getIsEnableAuthToggle } from '@/src/utils/env/get-auth-toggle';
 import { DialRole } from '@/src/models/dial/role';
 import { filterNames } from '@/src/utils/entities/filter-names';
 import { DEFAULT_ETAG } from '@/src/constants/api-headers';
+import { isValueTruthy } from '@/src/utils/types';
 
 export const dynamic = 'force-dynamic';
 
-export default async function Page(params: { params: Promise<{ id: string }> }) {
+export default async function Page(params: {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ code?: string; isUser?: string }>;
+}) {
   const token = await getUserToken(getIsEnableAuthToggle(), headers(), cookies());
 
   let etag = DEFAULT_ETAG;
@@ -24,6 +28,8 @@ export default async function Page(params: { params: Promise<{ id: string }> }) 
   let toolSets: Toolset[] | null = null;
 
   let roles: DialRole[] | null = null;
+  let oAuthCode = null;
+  let isUser = false;
 
   try {
     toolSet = await toolSetsApi.getToolset((await params.params).id, token, etag).then((res) => {
@@ -33,6 +39,9 @@ export default async function Page(params: { params: Promise<{ id: string }> }) 
 
     toolSets = await toolSetsApi.getToolsetList(token);
     roles = await rolesApi.getRolesList(token);
+    const searchParams = await params.searchParams;
+    oAuthCode = searchParams.code;
+    isUser = isValueTruthy(searchParams.isUser);
     if (toolSet === void 0 || toolSets === void 0 || roles === void 0) {
       return <Page403 />;
     }
@@ -48,7 +57,14 @@ export default async function Page(params: { params: Promise<{ id: string }> }) 
 
   return (
     <SaveValidationContextProvider>
-      <ToolsetView names={names} originalToolset={toolSet} roles={roles} etag={etag} />
+      <ToolsetView
+        oAuthCode={oAuthCode}
+        isUserLevel={isUser}
+        names={names}
+        originalToolset={toolSet}
+        roles={roles}
+        etag={etag}
+      />
     </SaveValidationContextProvider>
   );
 }
--- apps/ai-dial-admin/src/server/entities/toolsets-api.ts
+++ apps/ai-dial-admin/src/server/entities/toolsets-api.ts
@@ -1,16 +1,19 @@
 import { JWT } from 'next-auth/jwt';
 
-import { Toolset, Tool } from '@/src/models/dial/toolset';
+import { Toolset, Tool, ToolsetAuthCredentialLevel } from '@/src/models/dial/toolset';
 import { API } from '../api';
 import { BaseApi } from '../base-api';
 import { ServerActionResponse } from '@/src/models/server-action';
 import { DEFAULT_ETAG } from '@/src/constants/api-headers';
+import { getToolsetBasicBody, getToolsetSignInBody } from '../../utils/toolset/toolset-auth';
 
 export const TOOLSETS_URL = `${API}/toolSets`;
 export const TOOLSET_URL = (name?: string) => `${TOOLSETS_URL}/${name || ''}`;
 export const CORE_TOOLSET_URL = (name: string) => `${TOOLSETS_URL}/core/${name}`;
 export const TOOLS_URL = (name: string) => `${TOOLSET_URL(name)}/discovered-tools`;
 export const TOOLS_TRY_OUT_URL = (name: string) => `${TOOLSET_URL(name)}/call-tool`;
+export const TOOLSET_SIGN_IN_URL = `${TOOLSETS_URL}/sign-in`;
+export const TOOLSET_SIGN_OUT_URL = `${TOOLSETS_URL}/call-tool`;
 
 export class ToolsetsApi extends BaseApi {
   getToolsetList(token: JWT | null): Promise<Toolset[] | null> {
@@ -31,6 +34,20 @@ export class ToolsetsApi extends BaseApi {
     token: JWT | null,
   ): Promise<ServerActionResponse<Record<string, unknown>>> {
     return this.postAction(TOOLS_TRY_OUT_URL(name), body, token);
+  }
+
+  signInToolset(
+    toolset: Toolset,
+    type: ToolsetAuthCredentialLevel,
+    token: JWT | null,
+    apiKey?: string,
+    authCode?: string,
+  ) {
+    return this.postAction(TOOLSET_SIGN_IN_URL, getToolsetSignInBody(toolset, type, apiKey, authCode), token);
+  }
+
+  signOutToolset(toolset: Toolset, type: ToolsetAuthCredentialLevel, token: JWT | null) {
+    return this.postAction(TOOLSET_SIGN_IN_URL, getToolsetBasicBody(toolset, type), token);
   }
 
   removeToolset(token: JWT | null, name?: string): Promise<ServerActionResponse> {
