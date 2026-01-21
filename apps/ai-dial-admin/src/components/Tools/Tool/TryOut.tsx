2ab942459c46fc6fd3865c8af2ceb32ebcb90963
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2026-01-21 13:14:36 +0300

    feat: [Toolsets] Implement Tools try out (Issue #638) (#1777)

--- apps/ai-dial-admin/src/components/Tools/Tool/TryOut.tsx
+++ apps/ai-dial-admin/src/components/Tools/Tool/TryOut.tsx
@@ -61,7 +61,7 @@ const TryOut: FC<Props> = ({ tool, toolSetName, isAssetToolset }) => {
   }, [t]);
 
   const isEmptyRequest = useMemo(() => {
-    return tool?.inputSchema && Object.keys(tool?.inputSchema).length === 0;
+    return !tool?.inputSchema || (tool?.inputSchema && Object.keys(tool?.inputSchema.properties).length === 0);
   }, [tool?.inputSchema]);
 
   const sendRequest = useCallback(() => {
@@ -115,11 +115,11 @@ const TryOut: FC<Props> = ({ tool, toolSetName, isAssetToolset }) => {
               options={items}
               value={responseView}
               onChange={(v) => setResponseView(v as ParamsView)}
-              disabled={!isEmptyRequest}
+              disabled={isEmptyRequest}
             />
           </div>
           <div className="flex-1 basis-0 min-h-0 overflow-y-auto flex flex-col">
-            {!isEmptyRequest ? (
+            {isEmptyRequest ? (
               <div className="flex-1 flex items-center justify-center">{t(EntitiesI18nKey.NoInputs)}</div>
             ) : responseView === ParamsView.FORM ? (
               <SchemaUiRenderer
