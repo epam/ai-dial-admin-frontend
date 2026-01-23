d5b001651d838abe977800db8ad98a22ec8a77f9
Yauheni Osipau <yauheni_osipau@epam.com>
2026-01-23 10:21:15 +0300

    fix: remove unused props from scheme

--- apps/ai-dial-admin/src/components/Applications/ParametersTab/ParametersTab.tsx
+++ apps/ai-dial-admin/src/components/Applications/ParametersTab/ParametersTab.tsx
@@ -211,7 +211,14 @@ const ApplicationParametersTab: FC<Props> = ({
           ) : (
             <div className="flex-1 min-h-0 p-4 bg-layer-0">
               <SchemaUiRenderer
-                schema={scheme as RJSFSchema}
+                schema={
+                  {
+                    $defs: scheme.$defs,
+                    properties: scheme.properties,
+                    required: scheme.required,
+                    isRoot: true,
+                  } as RJSFSchema
+                }
                 data={entity?.applicationProperties}
                 onChangeConfiguration={onChangeConfiguration}
                 onGetSchemeDefaults={onGetSchemeDefaults}
