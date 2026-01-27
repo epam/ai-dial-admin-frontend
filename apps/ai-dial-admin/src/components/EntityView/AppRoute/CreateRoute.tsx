c7c271f860d1111a4607bb19632406610afec7e2
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2026-01-27 15:12:27 +0300

    fix: added validation for app route creation (#1903)

--- apps/ai-dial-admin/src/components/EntityView/AppRoute/CreateRoute.tsx
+++ apps/ai-dial-admin/src/components/EntityView/AppRoute/CreateRoute.tsx
@@ -1,21 +1,46 @@
 import { DialFormPopup, DialTextInputField, PopupSize } from '@epam/ai-dial-ui-kit';
-import { FC, useState } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
 
 import { ButtonsI18nKey, EntityFieldsI18nKey, EntityPlaceholdersI18nKey } from '@/src/constants/i18n';
 import { useI18n } from '@/src/locales/client';
+import { DialAppRoute } from '@/src/models/dial/route';
+import { FieldError } from '@/src/models/error';
 import { ApplicationRoute } from '@/src/types/routes';
 import { getCreateEntityTitle } from '@/src/utils/entities/create-entity';
+import { getErrorForName } from '@/src/utils/validation/name-error';
 
 interface Props {
   isModalOpen: boolean;
+  routes?: DialAppRoute[];
   onCreate: (name: string) => void;
   onClose: () => void;
 }
 
-const CreateRoute: FC<Props> = ({ isModalOpen, onClose, onCreate }) => {
+const CreateRoute: FC<Props> = ({ isModalOpen, routes, onClose, onCreate }) => {
   const t = useI18n();
 
   const [name, setName] = useState('');
+  const [nameError, setNameError] = useState<FieldError | null>(null);
+
+  const routeNames = useMemo(() => {
+    return routes?.map((r) => r.name || '');
+  }, [routes]);
+
+  const validateName = useCallback(
+    (name?: string) => {
+      const error = getErrorForName(name, routeNames, t, false, false, true);
+      setNameError(error);
+    },
+    [routeNames, t],
+  );
+
+  const onChangeName = useCallback(
+    (name?: string) => {
+      setName(name || '');
+      validateName(name);
+    },
+    [validateName],
+  );
 
   return (
     <DialFormPopup
@@ -29,7 +54,7 @@ const CreateRoute: FC<Props> = ({ isModalOpen, onClose, onCreate }) => {
       }}
       cancelLabel={t(ButtonsI18nKey.Cancel)}
       onCancel={onClose}
-      disableSubmitButton={!name}
+      disableSubmitButton={!name || !!nameError}
       portalId="CreateRoute"
       open={isModalOpen}
     >
@@ -39,7 +64,9 @@ const CreateRoute: FC<Props> = ({ isModalOpen, onClose, onCreate }) => {
           fieldTitle={t(EntityFieldsI18nKey.displayName)}
           placeholder={t(EntityPlaceholdersI18nKey.DisplayName)}
           value={name}
-          onChange={(name) => setName(name || '')}
+          onChange={onChangeName}
+          invalid={!!nameError}
+          errorText={nameError?.text}
         />
       </div>
     </DialFormPopup>
