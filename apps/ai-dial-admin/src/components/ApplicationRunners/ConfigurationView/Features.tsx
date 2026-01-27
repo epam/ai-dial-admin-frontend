5230b44a626167a7642373d588206d365dfad99b
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2026-01-27 14:41:14 +0100

    fix: duplicate modal padding (#1904)

--- apps/ai-dial-admin/src/components/ApplicationRunners/ConfigurationView/Features.tsx
+++ apps/ai-dial-admin/src/components/ApplicationRunners/ConfigurationView/Features.tsx
@@ -24,7 +24,7 @@ const AppRunnerFeatures: FC<Props> = ({ runner, onChangeRunner }) => {
   );
 
   return (
-    <div className="flex flex-col gap-y-8 h-full lg:w-[35%]">
+    <div className="flex flex-col gap-y-8 h-full">
       <ConfigurationEndpointControl
         endpoint={runner['dial:applicationTypeConfigurationEndpoint']}
         onChange={(value?: string) => {
