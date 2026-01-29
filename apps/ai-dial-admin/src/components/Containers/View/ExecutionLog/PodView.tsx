93011c1d696dd921748663ad4d3a64a20f7a3792
Siarhei Simanovich <Siarhei_Simanovich@epam.com>
2026-01-29 10:16:58 +0100

    feat(deployments): handle no pods returned in runnin container (Issue #1872)

--- apps/ai-dial-admin/src/components/Containers/View/ExecutionLog/PodView.tsx
+++ apps/ai-dial-admin/src/components/Containers/View/ExecutionLog/PodView.tsx
@@ -42,17 +42,17 @@ const PodView: FC<Props> = ({ pod, containerId }) => {
 
   return (
     <div className="flex flex-col flex-1 min-h-0 h-full gap-4">
-      {!!podData.restartCount && (
+      {!!podData?.restartCount && (
         <div className="flex gap-10">
-          <LabelledText label={t(EntityFieldsI18nKey.Restarts)} text={podData.restartCount?.toString()} />
+          <LabelledText label={t(EntityFieldsI18nKey.Restarts)} text={podData?.restartCount?.toString()} />
           <LabelledText
             label={t(EntityFieldsI18nKey.LastRestartedAt)}
-            text={formatDateTimeToLocalString(podData.lastFinishedAt)}
+            text={formatDateTimeToLocalString(podData?.lastFinishedAt)}
           />
           <LabelledText
             className="max-w-[350px]"
             label={t(EntityFieldsI18nKey.LastReason)}
-            text={restartReasons[podData.lastTerminationReason as string]}
+            text={restartReasons[podData?.lastTerminationReason as string]}
           />
         </div>
       )}
