55b1735acdd0b8e4aa61b6313f49a7cfa8c8268b
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-02 15:48:02 +0100

    fix tests

--- apps/ai-dial-admin/src/components/TestSuites/List/List.spec.tsx
+++ apps/ai-dial-admin/src/components/TestSuites/List/List.spec.tsx
@@ -3,18 +3,18 @@ import TestSuitesList from './List';
 import { describe, expect, test } from 'vitest';
 import { EntitiesI18nKey, MenuI18nKey } from '@/src/constants/i18n';
 
-describe('TestSuitsList', () => {
+describe('TestSuitesList', () => {
   test('renders BaseEntityList with correct props', () => {
     const data = [{ id: '1', name: 'Route One' }, { id: '2', name: 'Route Two' }, { id: '3' }];
 
     render(<TestSuitesList data={data} />);
-    expect(screen.getByText(MenuI18nKey.TestSuits)).toBeInTheDocument();
+    expect(screen.getByText(MenuI18nKey.TestSuites)).toBeInTheDocument();
     expect(screen.getByRole('table')).toBeInTheDocument();
   });
 
   test('renders with empty data', () => {
     render(<TestSuitesList data={[]} />);
-    expect(screen.getByText(MenuI18nKey.TestSuits)).toBeInTheDocument();
-    expect(screen.getByText(EntitiesI18nKey.NoTestSuits)).toBeInTheDocument();
+    expect(screen.getByText(MenuI18nKey.TestSuites)).toBeInTheDocument();
+    expect(screen.getByText(EntitiesI18nKey.NoTestSuites)).toBeInTheDocument();
   });
 });
