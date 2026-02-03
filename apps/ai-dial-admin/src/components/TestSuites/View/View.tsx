eca85a2f1d5d4c57fe11735368ac0ad5101853d5
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-03 11:35:48 +0100

    fix lint

--- apps/ai-dial-admin/src/components/TestSuites/View/View.tsx
+++ apps/ai-dial-admin/src/components/TestSuites/View/View.tsx
@@ -1,35 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { FC, useCallback, useRef, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 
 import { DialTabs } from '@epam/ai-dial-ui-kit';
 import { cloneDeep } from 'lodash';
 
 import HeaderButtons from '@/src/components/EntityView/Header/HeaderButtons';
 import EntityJsonEditor from '@/src/components/EntityView/JsonEditor/JsonEditor';
-import { useNotification } from '@/src/context/NotificationContext';
-import { useSaveValidationContext } from '@/src/context/SaveValidationContext';
-import { useProtectedRequest } from '@/src/hooks/use-protected-request';
 import { useI18n } from '@/src/locales/client';
+import { TestSuite } from '@/src/models/evaluation/test-suite';
 import { ApplicationRoute } from '@/src/types/routes';
 import { getViewHeaderClassName } from '@/src/utils/entities/view';
-import { EntityViewTab, getTestSuiteTabs } from '@/src/utils/tabs/utils';
-import { TestSuite } from '@/src/models/evaluation/test-suite';
+import { EntityViewTab, getAdapterTabs } from '@/src/utils/tabs/utils';
 
 interface Props {
   names: string[];
   originalTestSuite: TestSuite;
 }
 
-const TestSuiteView: FC<Props> = ({ names, originalTestSuite }) => {
+const TestSuiteView: FC<Props> = ({ originalTestSuite }) => {
   const t = useI18n();
-  const router = useRouter();
-  const { showNotification } = useNotification();
-  const { dispatch } = useSaveValidationContext();
-  const getReqRef = useRef(useProtectedRequest());
 
-  const tabs = getTestSuiteTabs(t);
+  const tabs = getAdapterTabs(t);
 
   const [activeTab, setActiveTab] = useState(EntityViewTab.Properties);
   const [selectedTestSuite, setSelectedTestSuite] = useState(cloneDeep(originalTestSuite));
@@ -51,7 +44,7 @@ const TestSuiteView: FC<Props> = ({ names, originalTestSuite }) => {
             <DialTabs tabs={tabs} activeTab={activeTab} onClick={onChangeActiveTab} />
           </div>
         )}
-        <HeaderButtons
+        {/* <HeaderButtons
           view={ApplicationRoute.TestSuites}
           entity={selectedTestSuite}
           isChanged={isChanged}
@@ -62,9 +55,9 @@ const TestSuiteView: FC<Props> = ({ names, originalTestSuite }) => {
           // onToggleJsonEditor={onToggleJsonEditor}
           // selectedFormat={selectedFormat}
           // onChangeSelectedFormat={setSelectedFormat}
-        />
+        /> */}
       </div>
-      <div className="flex-1 overflow-auto min-h-0">
+      {/* <div className="flex-1 overflow-auto min-h-0">
         {isJsonEditorEnabled ? (
           <EntityJsonEditor
             entity={selectedTestSuite}
@@ -72,14 +65,14 @@ const TestSuiteView: FC<Props> = ({ names, originalTestSuite }) => {
             setIsChanged={setIsChanged}
           />
         ) : (
-          <>
-            {activeTab === EntityViewTab.Properties && <div>Properties</div>}
-            {activeTab === EntityViewTab.TestCases && <div>Test Cases</div>}
-            {activeTab === EntityViewTab.Runs && <div>Runs</div>}
-            {activeTab === EntityViewTab.Trends && <div>Trends</div>}
-          </>
+          // <>
+          //   {activeTab === EntityViewTab.Properties && <div>Properties</div>}
+          //   {activeTab === EntityViewTab.TestCases && <div>Test Cases</div>}
+          //   {activeTab === EntityViewTab.Runs && <div>Runs</div>}
+          //   {activeTab === EntityViewTab.Trends && <div>Trends</div>}
+          // </>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
