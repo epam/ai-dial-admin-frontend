b71ee4a904c95c99477676ab6a0dbd0f2350f373
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-03 23:36:50 +0100

    feature: Adapters -  Change id section placement

--- apps/ai-dial-admin/src/components/Adapter/View/View.tsx
+++ apps/ai-dial-admin/src/components/Adapter/View/View.tsx
@@ -10,11 +10,7 @@ import { cloneDeep } from 'lodash';
 
 import { removeAdapter, updateAdapter } from '@/src/app/[lang]/adapters/actions';
 import { createModel } from '@/src/app/[lang]/models/actions';
-import AdapterModels from '@/src/components/Adapter/ModelsView/AdapterModels';
 import CreateEntity from '@/src/components/EntityListView/CreateEntity/CreateEntity';
-import EntityAudit from '@/src/components/EntityView/Audit/EntityAudit';
-import EntityHeader from '@/src/components/EntityView/Header/Header';
-import HeaderButtons from '@/src/components/EntityView/Header/HeaderButtons';
 import EntityJsonEditor from '@/src/components/EntityView/JsonEditor/JsonEditor';
 import { SOURCE_TYPE } from '@/src/components/SourceField/types';
 import { ButtonsI18nKey, CreateI18nKey } from '@/src/constants/i18n';
@@ -27,13 +23,12 @@ import { DialAdapter } from '@/src/models/dial/adapter';
 import { DialModelType } from '@/src/models/dial/model';
 import { ApplicationRoute } from '@/src/types/routes';
 import { getUpdateNotificationDescription, getUpdateNotificationTitle } from '@/src/utils/entities/update-entity';
-import { getViewHeaderClassName } from '@/src/utils/entities/view';
 import { isEqualSkippingUndefined } from '@/src/utils/is-equals-entity';
 import { getEndpointPostfix } from '@/src/utils/models/model-endpoint';
 import { getErrorNotification, getSuccessNotification } from '@/src/utils/notification';
 import { EntityViewTab, getAdapterTabs } from '@/src/utils/tabs/utils';
-import AdapterProperties from './AdapterProperties';
-import Tabs from '@/src/components/EntityHeaderControls/Tabs/HeaderTabs';
+import SimpleEntityHeader from '@/src/components/EntityHeaderControls/SimpleHeader';
+import TabsContent from './TabsContent';
 
 interface Props {
   etag: string;
@@ -53,8 +48,7 @@ const AdapterView: FC<Props> = ({ originalAdapter, modelsNames, etag }) => {
   const [activeTab, setActiveTab] = useState(EntityViewTab.Properties);
   const [selectedAdapter, setSelectedAdapter] = useState(cloneDeep(originalAdapter));
   const [isChanged, setIsChanged] = useState(false);
-  const [isJsonEditorEnabled, setIsJsonEditorEnabled] = useState(false);
-  const [key, setKey] = useState(0);
+  const [isEditorEnabled, setIsEditorEnabled] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
@@ -66,15 +60,12 @@ const AdapterView: FC<Props> = ({ originalAdapter, modelsNames, etag }) => {
   }, [selectedAdapter, originalAdapter]);
 
   const onDiscard = useCallback(() => {
-    if (isJsonEditorEnabled) {
+    if (isEditorEnabled) {
       dispatch({ type: ValidationActionType.SetJsonEditor, errors: [] });
-      setIsChanged(false);
-      // Due to we can't set invalid JSON as variable, we can't update entity in error state.
-      // Force JSON Editor re-render to show originalEntity on discard.
-      setKey((prevKey) => prevKey + 1);
     }
+
     setSelectedAdapter(originalAdapter);
-  }, [isJsonEditorEnabled, originalAdapter, dispatch]);
+  }, [isEditorEnabled, originalAdapter, dispatch]);
 
   const onChangeAdapter = useCallback(
     (entity: DialAdapter) => {
@@ -83,9 +74,9 @@ const AdapterView: FC<Props> = ({ originalAdapter, modelsNames, etag }) => {
     [setSelectedAdapter],
   );
 
-  const onToggleJsonEditor = useCallback(() => {
-    setIsJsonEditorEnabled((prev) => !prev);
-  }, [setIsJsonEditorEnabled]);
+  const onToggleEditor = useCallback(() => {
+    setIsEditorEnabled((prev) => !prev);
+  }, [setIsEditorEnabled]);
 
   const onSave = useCallback(() => {
     getReqRef.current(updateAdapter, selectedAdapter, etag).then((res) => {
@@ -105,59 +96,37 @@ const AdapterView: FC<Props> = ({ originalAdapter, modelsNames, etag }) => {
 
   return (
     <div className="flex flex-col flex-1 min-h-0 w-full bg-layer-2 rounded p-4 pb-14 lg:pb-4 relative">
-      <div className={getViewHeaderClassName(isJsonEditorEnabled)}>
-        <Tabs
-          tabs={tabs}
-          isEditorEnabled={isJsonEditorEnabled}
-          activeTab={activeTab}
-          onChangeActiveTab={setActiveTab}
+      <SimpleEntityHeader
+        view={ApplicationRoute.Adapters}
+        entity={selectedAdapter}
+        isChanged={isChanged}
+        onDiscard={onDiscard}
+        onSave={onSave}
+        tabs={tabs}
+        isEditorEnabled={isEditorEnabled}
+        activeTab={activeTab}
+        onChangeActiveTab={setActiveTab}
+        onToggleEditor={onToggleEditor}
+        onRemove={removeAdapter}
+      >
+        <DialNeutralButton
+          label={`${t(ButtonsI18nKey.Create)} ${t(CreateI18nKey.Model)}`}
+          iconBefore={<IconPlus {...BASE_BUTTON_ICON_PROPS} />}
+          onClick={() => setIsModalOpen(true)}
         />
+      </SimpleEntityHeader>
 
-        <HeaderButtons
-          view={ApplicationRoute.Adapters}
-          entity={selectedAdapter}
-          isChanged={isChanged}
-          onDiscard={onDiscard}
-          onSave={onSave}
-          onRemove={removeAdapter}
-          isEditorEnabled={isJsonEditorEnabled}
-          onToggleEditor={onToggleJsonEditor}
-        >
-          <DialNeutralButton
-            label={`${t(ButtonsI18nKey.Create)} ${t(CreateI18nKey.Model)}`}
-            iconBefore={<IconPlus {...BASE_BUTTON_ICON_PROPS} />}
-            onClick={() => setIsModalOpen(true)}
-          />
-        </HeaderButtons>
-      </div>
       <div className="flex-1 overflow-auto min-h-0">
-        {isJsonEditorEnabled ? (
+        {isEditorEnabled ? (
           <EntityJsonEditor
-            key={key}
             entity={selectedAdapter}
             setSelectedEntity={setSelectedAdapter}
             setIsChanged={setIsChanged}
           />
         ) : (
           <>
-            {activeTab === EntityViewTab.Properties && (
-              <div className="flex flex-col">
-                <EntityHeader entity={selectedAdapter} />
-                <div className="flex-1 min-h-0 pt-8">
-                  <AdapterProperties
-                    entity={selectedAdapter}
-                    onChangeAdapter={onChangeAdapter}
-                    isEntityImmutable={true}
-                  />
-                </div>
-              </div>
-            )}
-            {activeTab === EntityViewTab.Models && (
-              <AdapterModels adapter={selectedAdapter} onChangeAdapter={onChangeAdapter} />
-            )}
-            {activeTab === EntityViewTab.Audit && (
-              <EntityAudit entity={selectedAdapter} view={ApplicationRoute.Adapters} />
-            )}
+            <TabsContent activeTab={activeTab} selectedAdapter={selectedAdapter} onChangeAdapter={onChangeAdapter} />
+
             {isModalOpen &&
               createPortal(
                 <CreateEntity
---
+++ apps/ai-dial-admin/src/components/EntityHeaderControls/SimpleHeader.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+import { TabModel } from '@epam/ai-dial-ui-kit';
+
+import Tabs from '@/src/components/EntityHeaderControls/Tabs/HeaderTabs';
+import { ServerActionResponse } from '@/src/models/server-action';
+import { ApplicationRoute } from '@/src/types/routes';
+import { getHeaderClassName } from '@/src/utils/entities/view';
+import { EntityViewTab } from '@/src/utils/tabs/utils';
+import ReadonlyId from '../BaseControls/Id/ReadonlyId';
+import SimpleButtonsWrapper from './Wrappers/SimpleButtonsWrapper';
+
+interface Props<T> {
+  entity: T;
+  etag?: string;
+  view: ApplicationRoute;
+  tabs: TabModel[];
+  activeTab: EntityViewTab;
+  isEditorEnabled?: boolean;
+  children?: ReactNode;
+  isChanged: boolean;
+
+  onChangeActiveTab: (tab: EntityViewTab) => void;
+  onToggleEditor?: () => void;
+  onDiscard: () => void;
+  onSave: () => void;
+  onRemove: (entity: string) => Promise<ServerActionResponse>;
+}
+
+const SimpleEntityHeader = <T extends { name?: string }>({
+  isEditorEnabled,
+  children,
+  tabs,
+  activeTab,
+  onChangeActiveTab,
+  ...props
+}: Props<T>) => {
+  return (
+    <div className="flex flex-col gap-y-4 mb-8">
+      <div className={getHeaderClassName(isEditorEnabled)}>
+        <ReadonlyId value={props.entity.name || ''} />
+        <SimpleButtonsWrapper isEditorEnabled={isEditorEnabled} {...props}>
+          {children}
+        </SimpleButtonsWrapper>
+      </div>
+      <Tabs isEditorEnabled={isEditorEnabled} tabs={tabs} activeTab={activeTab} onChangeActiveTab={onChangeActiveTab} />
+    </div>
+  );
+};
+
+export default SimpleEntityHeader;
