ca36e7030f896fdd87d407603925485fc049de90
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-03 23:55:00 +0100

    feature: InterceptorTemplate - Change id section placement

---
+++ apps/ai-dial-admin/src/components/InterceptorTemplates/View/TabsContent.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { FC } from 'react';
+
+import EntityAudit from '@/src/components/EntityView/Audit/EntityAudit';
+import Interceptors from '@/src/components/InterceptorTemplates/View/Interceptors/Interceptors';
+import { InterceptorTemplate } from '@/src/models/interceptor-template';
+import { ApplicationRoute } from '@/src/types/routes';
+import { EntityViewTab } from '@/src/utils/tabs/utils';
+import PropertiesTabContent from '../Properties/TabContent';
+
+interface Props {
+  activeTab: EntityViewTab;
+  selectedTemplate: InterceptorTemplate;
+  onChange: (adapter: InterceptorTemplate) => void;
+}
+
+const TabsContent: FC<Props> = ({ activeTab, selectedTemplate, onChange }) => {
+  return (
+    <>
+      {activeTab === EntityViewTab.Properties && (
+        <PropertiesTabContent selectedTemplate={selectedTemplate} onChange={onChange} />
+      )}
+      {activeTab === EntityViewTab.Interceptors && <Interceptors interceptorList={selectedTemplate.interceptors} />}
+      {activeTab === EntityViewTab.Audit && (
+        <EntityAudit entity={selectedTemplate} view={ApplicationRoute.InterceptorTemplates} />
+      )}
+    </>
+  );
+};
+
+export default TabsContent;
