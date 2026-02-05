0e449f91742ef1ef09e78a306fcee6be8a7b3140
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-05 16:55:13 +0100

    feature: Test Suites - support test cases

--- apps/ai-dial-admin/src/components/TestSuites/TestCases/Header.tsx
+++ apps/ai-dial-admin/src/components/TestSuites/TestCases/Header.tsx
@@ -1,34 +1,20 @@
 'use client';
 
-import { FC, MouseEvent, useCallback, useMemo, useState } from 'react';
+import { FC, useMemo, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import {
   ButtonAppearance,
   ButtonVariant,
   DialButtonDropdown,
-  DialGhostButton,
   DialPrimaryButton,
   DropdownItem,
 } from '@epam/ai-dial-ui-kit';
-import { IconColumns2, IconPlus } from '@tabler/icons-react';
-import { GridApi } from 'ag-grid-community';
-import { useRouter } from 'next/navigation';
+import { IconPlus } from '@tabler/icons-react';
 
-import ResetFiltersButton from '@/src/components/ListView/Header/ResetFiltersButton';
-import CreateTestSuite from '@/src/components/TestSuites/Modals/Create/CreateTestSuite';
 import { ButtonsI18nKey, TestSuitesI18nKey } from '@/src/constants/i18n';
 import { BASE_BUTTON_ICON_PROPS } from '@/src/constants/main-layout';
-import { useNotification } from '@/src/context/NotificationContext';
-import { SaveValidationContextProvider } from '@/src/context/SaveValidationContext';
-import { useIsTabletScreen } from '@/src/hooks/use-is-tablet-screen';
 import { useI18n } from '@/src/locales/client';
-import { TestSuite } from '@/src/models/evaluation/test-suite';
-import { ServerActionResponse } from '@/src/models/server-action';
-import { ApplicationRoute } from '@/src/types/routes';
-import { getCreateNotificationDescription, getCreateNotificationTitle } from '@/src/utils/entities/create-entity';
-import { getErrorNotification, getSuccessNotification } from '@/src/utils/notification';
-import { getUrnForEntity } from '@/src/utils/open-in-new-tab';
 import AddTestCase from './AddTestCase';
 import ImportFileModal from './Import/ImportFile';
 
