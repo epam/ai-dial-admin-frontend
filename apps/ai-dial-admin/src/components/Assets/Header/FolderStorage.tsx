c9a78b80da6ba80a37b5986d4d2cd2976fa48439
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-07 00:51:27 +0100

    feature: [Entity view] Change id section placement

--- apps/ai-dial-admin/src/components/Assets/Header/FolderStorage.tsx
+++ apps/ai-dial-admin/src/components/Assets/Header/FolderStorage.tsx
@@ -8,11 +8,12 @@ import { BASE_BUTTON_ICON_PROPS } from '@/src/constants/main-layout';
 import { useCurrentLocale, useI18n } from '@/src/locales/client';
 import { AssetWithVersion } from '@/src/models/dial/deployment-asset';
 import { DialFile } from '@/src/models/dial/file';
+import { Publication } from '@/src/models/dial/publications';
 import { ApplicationRoute } from '@/src/types/routes';
 import { removeTrailingSlash } from '@/src/utils/files/path';
 
 interface Props {
-  asset: AssetWithVersion | DialFile;
+  asset: AssetWithVersion | DialFile | Publication;
 }
 
 const FoldersStorageLabel: FC<Props> = ({ asset }) => {
