daa2e61ff4962ab64163ba29438d36fd2af184fc
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-08 21:51:27 +0100

    chore: update selected file

--- apps/ai-dial-admin/src/constants/i18n.ts
+++ apps/ai-dial-admin/src/constants/i18n.ts
@@ -274,6 +274,7 @@ export enum ButtonsI18nKey {
   Update = 'Buttons.Update',
   Cancel = 'Buttons.Cancel',
   Delete = 'Buttons.Delete',
+  Change = 'Buttons.Change',
   DeleteAll = 'Buttons.DeleteAll',
   Duplicate = 'Buttons.Duplicate',
   Save = 'Buttons.Save',
@@ -1285,4 +1286,5 @@ export enum TestSuitesI18nKey {
   FromDial = 'TestSuites.FromDial',
   Other = 'TestSuites.Other',
   ChatInterface = 'TestSuites.ChatInterface',
+  Preview = 'TestSuites.Preview',
 }
--- apps/ai-dial-admin/src/locales/en.ts
+++ apps/ai-dial-admin/src/locales/en.ts
@@ -363,6 +363,7 @@ export default {
     Duplicate: 'Duplicate',
     Create: 'Create',
     Update: 'Update',
+    Change: 'Change',
     Save: 'Save',
     SaveAndRedeploy: 'Save & redeploy',
     Move: 'Move',
@@ -1306,5 +1307,6 @@ export default {
     ImportFromPC: 'Import from PC storage',
     Other: 'Other',
     ChatInterface: 'Chat interface',
+    Preview: 'Preview',
   },
 } as const;
