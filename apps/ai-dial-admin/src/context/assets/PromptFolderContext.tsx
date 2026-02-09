f88446cb9b548b7f824086ce85a088c0ba5f4d57
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-09 11:20:23 +0100

    fix alias

--- apps/ai-dial-admin/src/context/assets/PromptFolderContext.tsx
+++ apps/ai-dial-admin/src/context/assets/PromptFolderContext.tsx
@@ -2,7 +2,7 @@
 
 import { getPrompts } from '@/src/app/[lang]/prompts/actions';
 import { DialPrompt } from '@/src/models/dial/prompt';
-import { createFolderContext } from './AssetsFolderContext';
+import { createFolderContext } from '@/src/context/assets/AssetsFolderContext';
 
 export const { Provider: PromptFolderProvider, useFolderContext: usePromptFolder } = createFolderContext(
   getPrompts as (path: string) => Promise<DialPrompt[] | null | undefined>,
--- apps/ai-dial-admin/src/context/assets/ToolsetsFolderContext.tsx
+++ apps/ai-dial-admin/src/context/assets/ToolsetsFolderContext.tsx
@@ -2,7 +2,7 @@
 
 import { getToolsets } from '@/src/app/[lang]/assets-toolsets/actions';
 import { AssetToolset } from '@/src/models/dial/deployment-asset';
-import { createFolderContext } from './AssetsFolderContext';
+import { createFolderContext } from '@/src/context/assets/AssetsFolderContext';
 
 export const { Provider: ToolsetFolderProvider, useFolderContext: useToolsetFolder } = createFolderContext(
   getToolsets as (path: string) => Promise<AssetToolset[] | null | undefined>,
