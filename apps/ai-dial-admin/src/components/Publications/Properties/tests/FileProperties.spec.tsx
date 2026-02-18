c11227fbb674dd95e54cb719e1815c88fda2d066
Yauheni Osipau <yauheni_osipau@epam.com>
2026-02-18 11:16:05 +0300

    fix test

--- apps/ai-dial-admin/src/components/Publications/Properties/tests/FileProperties.spec.tsx
+++ apps/ai-dial-admin/src/components/Publications/Properties/tests/FileProperties.spec.tsx
@@ -24,7 +24,7 @@ describe('FileProperties', () => {
   });
   test('renders files list title and files', () => {
     const publication = {
-      files: [{ name: 'file1.txt' }, { name: 'file2.txt' }],
+      files: [{ file: { name: 'file1.txt' } }, { file: { name: 'file2.txt' } }],
       action: 'download',
     };
 
