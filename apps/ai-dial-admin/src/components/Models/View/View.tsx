a6695b57514f8d563ad119c3cceeeef33cc58189
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2026-02-20 16:04:22 +0300

    fix: [Model] Deleting model redirect to Applications page (Issue #2265) (#2266)

--- apps/ai-dial-admin/src/components/Models/View/View.tsx
+++ apps/ai-dial-admin/src/components/Models/View/View.tsx
@@ -200,7 +200,7 @@ const View: FC<Props> = ({ originalModel, etag, ...props }) => {
     <>
       <div className="flex flex-col flex-1 min-h-0 w-full bg-layer-2 rounded p-4 pb-14 lg:pb-4 relative">
         <SimpleEntityHeader
-          view={ApplicationRoute.Applications}
+          view={ApplicationRoute.Models}
           entity={selectedModel}
           isChanged={isChanged}
           onDiscard={onTryToDiscard}
