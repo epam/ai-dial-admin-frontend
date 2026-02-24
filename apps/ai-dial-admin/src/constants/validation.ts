d92465343d672a7905692e8578792a9f94db1b4d
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-02-25 00:07:06 +0100

    fix: The minimum number of characters for an entity name must be at least one character

--- apps/ai-dial-admin/src/constants/validation.ts
+++ apps/ai-dial-admin/src/constants/validation.ts
@@ -1,6 +1,6 @@
 export const MAX_NAME_SYMBOLS = 255;
 export const MAX_DEPLOYMENT_ID_SYMBOLS = 36;
-export const MIN_NAME_SYMBOLS = 2;
+export const MIN_NAME_SYMBOLS = 1;
 export const MAX_URL_ID_SYMBOLS = 850;
 export const MAX_DESCRIPTION_SYMBOLS = 2048;
 export const FORBIDDEN_NAME_SYMBOLS = ['%', '/', '\\', ';'];
