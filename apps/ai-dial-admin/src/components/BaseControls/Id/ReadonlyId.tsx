0bc626a001952fc3e08b0e85af1eb982377bb33f
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2026-02-24 18:33:45 +0100

    fix: Entity ID is unnecessarily truncated in header

--- apps/ai-dial-admin/src/components/BaseControls/Id/ReadonlyId.tsx
+++ apps/ai-dial-admin/src/components/BaseControls/Id/ReadonlyId.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 
+import { DialEllipsisTooltip } from '@epam/ai-dial-ui-kit';
+
+import CopyButton from '@/src/components/Common/CopyButton/CopyButton';
 import LabelledText from '@/src/components/Common/LabelledText/LabelledText';
 import { EntityFieldsI18nKey } from '@/src/constants/i18n';
 import { useI18n } from '@/src/locales/client';
@@ -12,7 +15,14 @@ const ReadonlyId: FC<Props> = ({ value }) => {
   const t = useI18n();
 
   return (
-    <LabelledText copyLabel={t(EntityFieldsI18nKey.id)} text={value || ''} copyable={true} className="!max-w-none" />
+    <div className="flex-1 min-w-0">
+      <LabelledText copyLabel={t(EntityFieldsI18nKey.id)} copyable={true}>
+        <div className="flex flex-row gap-x-3 items-center">
+          <DialEllipsisTooltip text={value || ''} />
+          <CopyButton value={value || ''} valueLabel={t(EntityFieldsI18nKey.id)} />
+        </div>
+      </LabelledText>
+    </div>
   );
 };
 
