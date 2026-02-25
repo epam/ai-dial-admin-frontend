1f47639cf541d65858417f4013f60ee5f47cd2b7
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2026-02-25 15:16:26 +0100

    fix test

--- apps/ai-dial-admin/src/components/Publications/View/tests/Properties.spec.tsx
+++ apps/ai-dial-admin/src/components/Publications/View/tests/Properties.spec.tsx
@@ -1,10 +1,10 @@
-import { EntitiesI18nKey, EntityFieldsI18nKey } from '@/src/constants/i18n';
+import { EntitiesI18nKey } from '@/src/constants/i18n';
 import { ApplicationRoute } from '@/src/types/routes';
 import { render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 import PublicationProperties from '../Properties';
 
-const fakePublication = { id: '1', name: 'Test Publication', createdAt: '2020-12-12T22:22' } as any;
+const fakePublication = { id: '1', folderId: 'id', name: 'Test Publication', createdAt: '2020-12-12T22:22' } as any;
 const fakeSchemes = [{ id: 'scheme1' }] as any;
 
 describe('PublicationProperties', () => {
