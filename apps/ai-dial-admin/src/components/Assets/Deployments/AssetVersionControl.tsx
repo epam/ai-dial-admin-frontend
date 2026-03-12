01746c43005963baeb7907df38ec5b1893e9c403
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-12 12:23:10 +0100

    feat: review add version modal

--- apps/ai-dial-admin/src/components/Assets/Deployments/AssetVersionControl.tsx
+++ apps/ai-dial-admin/src/components/Assets/Deployments/AssetVersionControl.tsx
@@ -150,6 +150,8 @@ const AssetVersionControl: FC<Props> = ({
           <AddVersionModal
             header={t(PromptsI18nKey.NewVersionCreate)}
             isModalOpen={isModalOpen}
+            initialVersion={asset.version}
+            entityName={asset.name}
             existingVersions={getVersionsPerName(assets || [])}
             // existingVersions={[...versions, ...addedVersions]}
             onClose={handleModalClose}
