499649f8afac2142bb07ea7e86f4567c5ce7b199
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-16 20:28:28 +0100

    fix: [Entities][Models] Validation missing when editing Display Version in Models

--- apps/ai-dial-admin/src/utils/entities/filter-names.ts
+++ apps/ai-dial-admin/src/utils/entities/filter-names.ts
@@ -23,12 +23,16 @@ export const getNamesConfigurations = (namesWithVersions: string[]) => {
 export const filterDisplayNamesWithVersions = (entities?: DialModel[] | null, currentModel?: DialModel): string[] => {
   return (
     (entities?.reduce((acc, curr) => {
-      if (
-        curr.displayName != null &&
-        curr.displayName !== currentModel?.displayName &&
-        (currentModel?.displayVersion ? curr.displayVersion !== currentModel?.displayVersion : true)
-      ) {
+      if (curr.displayName != null && curr.displayName !== currentModel?.displayName) {
         acc.push(`${curr.displayName}___${curr.displayVersion || ''}`);
+      }
+
+      if (
+        curr.displayName === currentModel?.displayName &&
+        currentModel?.displayVersion &&
+        curr.displayVersion !== currentModel?.displayVersion
+      ) {
+         acc.push(`${curr.displayName}___${curr.displayVersion || ''}`);
       }
       return acc;
     }, [] as string[]) as string[]) || []
