630dbbdacf54fa60e922bf303d0d26fc23ea1bf6
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-24 23:54:03 +0100

    feat: add small correction for eval

--- apps/ai-dial-admin/src/components/TestSuites/View/TabsContent.tsx
+++ apps/ai-dial-admin/src/components/TestSuites/View/TabsContent.tsx
@@ -82,7 +82,7 @@ const TabsContent: FC<Props> = ({
         />
       )}
       {activeTab === EntityViewTab.Runs && <Runs selectedTestSuite={selectedTestSuite} runRefreshRef={runRefreshRef} />}
-      {activeTab === EntityViewTab.Metrics && <Metrics selectedTestSuite={selectedTestSuite} />}
+      {activeTab === EntityViewTab.Validations && <Metrics selectedTestSuite={selectedTestSuite} />}
     </>
   );
 };
