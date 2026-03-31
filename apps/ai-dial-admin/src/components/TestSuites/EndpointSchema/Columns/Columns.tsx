56eb3273280040b6a45e63153a5651358e52ef41
Yauheni Osipau <yauheni_osipau@epam.com>
2026-03-31 12:03:09 +0300

    added jsonAta doc

--- apps/ai-dial-admin/src/components/TestSuites/EndpointSchema/Columns/Columns.tsx
+++ apps/ai-dial-admin/src/components/TestSuites/EndpointSchema/Columns/Columns.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { FC, useCallback, useEffect, useMemo, useRef } from 'react';
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { createPortal } from 'react-dom';
 
-import { DialNeutralButton } from '@epam/ai-dial-ui-kit';
+import OpenPopup from '@/public/images/icons/open-pop-up.svg';
+import { ButtonAppearance, DialNeutralButton, DialPrimaryButton } from '@epam/ai-dial-ui-kit';
 import { IconPlus } from '@tabler/icons-react';
 import { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community';
 import { JSONSchema7 } from 'json-schema';
@@ -11,11 +13,12 @@ import GridView from '@/src/components/Grid/GridView/GridView';
 import { getColumnsGridColumns } from '@/src/components/TestSuites/utils/columns';
 import { ONE_ACTION_COLUMN } from '@/src/constants/ag-grid';
 import { getRemoveOperation } from '@/src/constants/grid-columns/actions';
-import { BasicI18nKey, ButtonsI18nKey, TestSuitesI18nKey } from '@/src/constants/i18n';
+import { BasicI18nKey, ButtonsI18nKey, CompareI18nKey, JsonAtaI18nKey, TestSuitesI18nKey } from '@/src/constants/i18n';
 import { BASE_BUTTON_ICON_PROPS } from '@/src/constants/main-layout';
 import { useSaveValidationContext, ValidationActionType } from '@/src/context/SaveValidationContext';
 import { useI18n } from '@/src/locales/client';
 import { ResponseColumn } from '@/src/models/evaluation/test-suite';
+import DocumentationModal from './DocumentationModal';
 
 interface ColumnsProps {
   responseColumns: ResponseColumn[];
@@ -30,6 +33,8 @@ const Columns: FC<ColumnsProps> = ({ responseColumns, onChangeResponseColumns, r
 
   const columnsRef = useRef(responseColumns);
   const gridApiRef = useRef<GridApi | null>(null);
+
+  const [isDocumentationModalOpen, setIsDocumentationModalOpen] = useState(false);
 
   useEffect(() => {
     columnsRef.current = responseColumns;
@@ -117,12 +122,20 @@ const Columns: FC<ColumnsProps> = ({ responseColumns, onChangeResponseColumns, r
     <div className="flex flex-col gap-4 flex-1 min-h-0">
       <div className="flex flex-row justify-between items-center">
         <span className="text-secondary small">{t(TestSuitesI18nKey.ColumnsDescription)}</span>
-        <DialNeutralButton
-          label={t(ButtonsI18nKey.Add)}
-          iconBefore={<IconPlus {...BASE_BUTTON_ICON_PROPS} />}
-          onClick={onAddColumn}
-          disabled={!isValid}
-        />
+        <div className="flex flex-row gap-2">
+          <DialPrimaryButton
+            iconBefore={<OpenPopup {...BASE_BUTTON_ICON_PROPS} />}
+            appearance={ButtonAppearance.Ghost}
+            label={`${t(CompareI18nKey.View)} ${t(JsonAtaI18nKey.JSONAtaDoc)}`}
+            onClick={() => setIsDocumentationModalOpen(true)}
+          />
+          <DialNeutralButton
+            label={t(ButtonsI18nKey.Add)}
+            iconBefore={<IconPlus {...BASE_BUTTON_ICON_PROPS} />}
+            onClick={onAddColumn}
+            disabled={!isValid}
+          />
+        </div>
       </div>
       <div className="flex-1 min-h-0 overflow-auto">
         <GridView<ResponseColumn>
@@ -131,6 +144,15 @@ const Columns: FC<ColumnsProps> = ({ responseColumns, onChangeResponseColumns, r
           onGridReady={onGridReady}
         />
       </div>
+      {isDocumentationModalOpen &&
+        createPortal(
+          <DocumentationModal
+            schema={responseSchema}
+            isModalOpen={isDocumentationModalOpen}
+            onClose={() => setIsDocumentationModalOpen(false)}
+          />,
+          document.body,
+        )}
     </div>
   );
 };
