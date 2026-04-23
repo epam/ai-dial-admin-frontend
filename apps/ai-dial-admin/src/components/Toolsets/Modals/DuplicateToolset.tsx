ea9383730650692e3c262167bf2caaf5b8560a34
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2026-04-23 16:28:56 +0300

    fix: [Entities --> Toolsets] Duplicate toolset can be created with an existing Display Name, but error appears in Properties tab after creation (Issue #3062) (#3068)

--- apps/ai-dial-admin/src/components/Toolsets/Modals/DuplicateToolset.tsx
+++ apps/ai-dial-admin/src/components/Toolsets/Modals/DuplicateToolset.tsx
@@ -1,8 +1,11 @@
-import { DialFormPopup } from '@epam/ai-dial-ui-kit';
 import { FC, useCallback, useEffect, useMemo, useState } from 'react';
 
+import { DialFormPopup } from '@epam/ai-dial-ui-kit';
+
+import { checkIsUniqueDeploymentName } from '@/src/app/actions';
 import DisplayNameControl from '@/src/components/BaseControls/DisplayName';
 import IdControl from '@/src/components/BaseControls/Id/Id';
+import ApiKeyHeaderControl from '@/src/components/Toolsets/Auth/Controls/ApiKeyHeaderControl';
 import OAuthAuthSectionControl from '@/src/components/Toolsets/Auth/Controls/OAuthAuthSectionControl';
 import { ButtonsI18nKey, ToolsetI18nKey } from '@/src/constants/i18n';
 import { useSaveValidationContext, ValidationActionType } from '@/src/context/SaveValidationContext';
@@ -10,7 +13,6 @@ import { useI18n } from '@/src/locales/client';
 import { Toolset, ToolsetAuthType } from '@/src/models/dial/toolset';
 import { ApplicationRoute } from '@/src/types/routes';
 import { getClonedEntityName, getCloneTitle } from '@/src/utils/entities/duplicate-entity';
-import ApiKeyHeaderControl from '../Auth/Controls/ApiKeyHeaderControl';
 
 interface Props {
   isModalOpen: boolean;
@@ -29,6 +31,7 @@ const DuplicateToolset: FC<Props> = ({ names, onDuplicate, isModalOpen, onClose,
     displayName: getClonedEntityName(entity.displayName, true),
   });
   const { isValid, dispatch } = useSaveValidationContext();
+  const [isUniqueNameError, setIsUniqueNameError] = useState<boolean | undefined>(void 0);
 
   const authType = useMemo(
     () => clonedEntity.authSettings?.authenticationType || ToolsetAuthType.NONE,
@@ -140,6 +143,21 @@ const DuplicateToolset: FC<Props> = ({ names, onDuplicate, isModalOpen, onClose,
     [clonedEntity],
   );
 
+  const onIdChange = useCallback(async (entity: Toolset) => {
+    setEntity(entity);
+    setIsUniqueNameError(false);
+  }, []);
+
+  const onDuplicateClick = useCallback(async () => {
+    const isUnique = await checkIsUniqueDeploymentName(clonedEntity.name as string);
+
+    setIsUniqueNameError(!isUnique);
+
+    if (!isUnique) return;
+
+    onDuplicate(clonedEntity);
+  }, [clonedEntity, onDuplicate]);
+
   return (
     <DialFormPopup
       onClose={onClose}
@@ -148,15 +166,20 @@ const DuplicateToolset: FC<Props> = ({ names, onDuplicate, isModalOpen, onClose,
       open={isModalOpen}
       cancelLabel={t(ButtonsI18nKey.Cancel)}
       submitLabel={t(ButtonsI18nKey.Duplicate)}
-      onSubmit={() => onDuplicate(clonedEntity)}
+      onSubmit={onDuplicateClick}
       onCancel={onClose}
       disableSubmitButton={!isValid}
     >
       <div className="flex flex-col px-6 py-4">
         <div className="flex flex-col gap-y-8">
-          <IdControl entity={clonedEntity} onChangeEntity={setEntity} names={names} />
+          <IdControl entity={clonedEntity} onChangeEntity={onIdChange} isUniqueNameError={isUniqueNameError} />
 
-          <DisplayNameControl displayName={clonedEntity.displayName} onChange={onChangeDisplayName} required />
+          <DisplayNameControl
+            displayName={clonedEntity.displayName}
+            onChange={onChangeDisplayName}
+            names={names}
+            required
+          />
 
           {authType !== ToolsetAuthType.NONE && (
             <h3>
