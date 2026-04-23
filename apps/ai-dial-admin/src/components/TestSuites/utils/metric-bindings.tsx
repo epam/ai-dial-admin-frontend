9e6f944c972053a300406eaaac692a55f9120b1e
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-04-23 15:40:46 +0200

    feat(metrics): add display name for metrics

--- apps/ai-dial-admin/src/components/TestSuites/utils/metric-bindings.tsx
+++ apps/ai-dial-admin/src/components/TestSuites/utils/metric-bindings.tsx
@@ -16,14 +16,12 @@ export const generateMetricDefaultInputBindings = (schema: JSONSchema7) => {
 
 export const generateMetricDefaultBindings = (
   name: string,
-  metricDisplayName: string,
   details: Metric,
   configBindings: MetricBinding[],
   inputBindings: MetricBinding[],
 ) => {
   return {
     name,
-    displayName: metricDisplayName,
     metricDeclarationId: details?.metricDeclarationId,
     metricDeclarationVersionId: details?.id,
     inputBindings:
