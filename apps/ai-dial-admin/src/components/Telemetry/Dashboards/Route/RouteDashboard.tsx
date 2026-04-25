d4b762bc0a7e9980d31e726c41a7526705f1cd56
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-04-25 14:42:59 +0200

    add conditional

--- apps/ai-dial-admin/src/components/Telemetry/Dashboards/Route/RouteDashboard.tsx
+++ apps/ai-dial-admin/src/components/Telemetry/Dashboards/Route/RouteDashboard.tsx
@@ -2,23 +2,23 @@ import { FC } from 'react';
 
 import { QueryInput } from '@/src/components/Telemetry/Dashboard';
 import RouteChartsValues from '@/src/components/Telemetry/Dashboards/Route/RouteChartsValues';
+import {
+  CALL_BY_DEPLOYMENT_COLUMNS,
+  CALL_BY_PARENT_DEPLOYMENT_COLUMNS,
+  CALL_BY_PROJECT_COLUMNS,
+  CALL_BY_ROUTES_COLUMNS,
+} from '@/src/constants/grid-columns/grid-columns';
 import { TelemetryI18nKey } from '@/src/constants/i18n';
 import {
   createRouteUsageQuery,
   ROUTE_DEPLOYMENT_QUERY,
   ROUTE_PARENT_DEPLOYMENT_QUERY,
   ROUTE_PROJECT_QUERY,
-  ROUTE_QUERY
+  ROUTE_QUERY,
 } from '@/src/constants/telemetry';
 import { useI18n } from '@/src/locales/client';
 import { ServerActionResponse } from '@/src/models/server-action';
 import { TelemetryQuery } from '@/src/models/telemetry';
-import {
-  CALL_BY_DEPLOYMENT_COLUMNS,
-  CALL_BY_PARENT_DEPLOYMENT_COLUMNS,
-  CALL_BY_PROJECT_COLUMNS,
-  CALL_BY_ROUTES_COLUMNS,
-} from '../../../../constants/grid-columns/grid-columns';
 import TelemetryGrid from '../../TelemetryGrid';
 import LineChart from '../LineChart/LineChart';
 
@@ -45,15 +45,17 @@ const RouteDashboard: FC<Props> = ({ getData, getToolsConsumptionData, refreshTi
       </div>
       <div className="flex flex-col w-full gap-6">
         <div className="flex flex-col md:flex-row gap-6">
-          <div className="flex flex-1 relative">
-            <TelemetryGrid
-              getData={getData}
-              refreshTime={refreshTime}
-              query={ROUTE_DEPLOYMENT_QUERY}
-              columnDefs={CALL_BY_DEPLOYMENT_COLUMNS}
-              title={t(TelemetryI18nKey.CallsByDeployment)}
-            />
-          </div>
+          {!isEntityView && (
+            <div className="flex flex-1 relative">
+              <TelemetryGrid
+                getData={getData}
+                refreshTime={refreshTime}
+                query={ROUTE_DEPLOYMENT_QUERY}
+                columnDefs={CALL_BY_DEPLOYMENT_COLUMNS}
+                title={t(TelemetryI18nKey.CallsByDeployment)}
+              />
+            </div>
+          )}
 
           <div className="flex flex-1 relative">
             <TelemetryGrid
