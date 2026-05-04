a3ac8f3fdaa13067f8c1ba5e9b768245dcf20cbe
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-05-04 14:38:58 +0200

    fix build

--- apps/ai-dial-admin/src/components/Common/FileManager/FileManager.tsx
+++ apps/ai-dial-admin/src/components/Common/FileManager/FileManager.tsx
@@ -7,8 +7,10 @@ import {
   DialDeletedItem,
   DialFile,
   DialFileManager,
+  DialRootFolder,
   DialUploadFileItem,
   FileManagerColumnKey,
+  FileManagerGridRow,
 } from '@epam/ai-dial-ui-kit';
 import { ColDef } from 'ag-grid-community';
 
@@ -31,7 +33,6 @@ import { ServerActionResponse } from '@/src/models/server-action';
 import { ApplicationRoute } from '@/src/types/routes';
 import { getFolderName } from '@/src/utils/files/folder';
 import { getErrorNotification, getSuccessNotification } from '@/src/utils/notification';
-import { DialRootFolder, FileManagerGridRow } from '@epam/ai-dial-ui-kit';
 import MoveItemsModal from './MoveItemsModal';
 import { MOVE_ITEMS_INDICATOR_DELAY } from './constants';
 import {
