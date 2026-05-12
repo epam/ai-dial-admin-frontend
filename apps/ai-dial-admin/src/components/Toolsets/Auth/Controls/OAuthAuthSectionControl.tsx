8e0b686bc416c13175f968ab4e86d18c90cc885e
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-05-12 16:09:48 +0200

    fix build

--- apps/ai-dial-admin/src/components/Toolsets/Auth/Controls/OAuthAuthSectionControl.tsx
+++ apps/ai-dial-admin/src/components/Toolsets/Auth/Controls/OAuthAuthSectionControl.tsx
@@ -41,7 +41,6 @@ const OAuthAuthSectionControl: FC<Props> = ({
         endpoint={authorizationEndpoint || ''}
         placeholder={t(EntityPlaceholdersI18nKey.AuthorizationEndpoint)}
         onChange={onChangeAuthorizationEndpoint}
-        isFullWidth
       />
 
       <EndpointControl
@@ -49,7 +48,6 @@ const OAuthAuthSectionControl: FC<Props> = ({
         required
         isFullWidth
         label={t(EntityFieldsI18nKey.tokenEndpoint)}
-        isFullWidth
         endpoint={tokenEndpoint || ''}
         placeholder={t(EntityPlaceholdersI18nKey.TokenEndpoint)}
         onChange={onChangeTokenEndpoint}
