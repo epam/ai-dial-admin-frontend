879e4a0ca1368b8c637b05df9f697daabc31bbe4
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-05-13 15:17:22 +0200

    feat(toolset): support login in both level

--- apps/ai-dial-admin/src/components/Toolsets/Auth/Sections/AuthHeader.tsx
+++ apps/ai-dial-admin/src/components/Toolsets/Auth/Sections/AuthHeader.tsx
@@ -20,6 +20,23 @@ export const AuthHeader: FC<Props> = ({ toolset }) => {
   const isUserLoggedIn = isUserLoggedInToToolset(toolset);
   const isAdminLoggedIn = isAdminLoggedInToToolset(toolset);
   const isAuthenticated = isUserLoggedIn && isAdminLoggedIn;
+
+  const getAuthStatusLabel = () => {
+    if (isAuthenticated) {
+      return t(ToolsetI18nKey.isAuthenticated);
+    }
+
+    if (isUserLoggedIn) {
+      return t(ToolsetI18nKey.UserLoggedIn);
+    }
+
+    if (isAdminLoggedIn) {
+      return t(ToolsetI18nKey.AdminLoggedIn);
+    }
+
+    return t(ToolsetI18nKey.LoggedOut);
+  };
+
   return (
     <LabelledText label={t(EntityFieldsI18nKey.authentication)}>
       <div className="flex items-center gap-2">
@@ -29,15 +46,7 @@ export const AuthHeader: FC<Props> = ({ toolset }) => {
             isUserLoggedIn || isAdminLoggedIn ? 'bg-accent-secondary' : 'bg-red-400',
           )}
         ></div>
-        <div>
-          {isAuthenticated
-            ? t(ToolsetI18nKey.isAuthenticated)
-            : isUserLoggedIn
-              ? t(ToolsetI18nKey.UserLoggedIn)
-              : isAdminLoggedIn
-                ? t(ToolsetI18nKey.AdminLoggedIn)
-                : t(ToolsetI18nKey.LoggedOut)}
-        </div>
+        <div>{getAuthStatusLabel()}</div>
       </div>
     </LabelledText>
   );
