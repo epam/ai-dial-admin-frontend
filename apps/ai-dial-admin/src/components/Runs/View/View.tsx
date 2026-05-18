fb6a6b0de15b68330b049b32a64eeff82b220a4e
Yauheni Osipau <72380335+ya092@users.noreply.github.com>
2026-05-19 00:08:48 +0300

    fix: [Eval] Run page redesign (#3354)

--- apps/ai-dial-admin/src/components/Runs/View/View.tsx
+++ apps/ai-dial-admin/src/components/Runs/View/View.tsx
@@ -1,19 +1,24 @@
 'use client';
 
-import { FC, useCallback, useState } from 'react';
+import { FC, useCallback, useMemo } from 'react';
 
 import Grafana from '@/public/images/icons/grafana.svg';
-import { Entity } from '@epam/ai-dial-shared';
-import { DialLinkButton } from '@epam/ai-dial-ui-kit';
+import { DialIconButton, DialLabelledText, DialLinkButton } from '@epam/ai-dial-ui-kit';
+import { IconExternalLink } from '@tabler/icons-react';
 
-import RunHeader from '@/src/components/EntityHeaderControls/RunHeader';
-import { RunsI18nKey } from '@/src/constants/i18n';
+import LabelledText from '@/src/components/Common/LabelledText/LabelledText';
+import RunStatusComponent from '@/src/components/Common/RunStatus/RunStatus';
+import EntityInfoHeader from '@/src/components/EntityHeaderControls/Info/InfoHeader';
+import SimpleEntityHeader from '@/src/components/EntityHeaderControls/SimpleHeader';
+import { EntityFieldsI18nKey, RunsI18nKey } from '@/src/constants/i18n';
+import { BASE_BUTTON_ICON_PROPS } from '@/src/constants/main-layout';
 import { useI18n } from '@/src/locales/client';
 import { Run } from '@/src/models/evaluation/run';
 import { ServerActionResponse } from '@/src/models/server-action';
 import { ApplicationRoute } from '@/src/types/routes';
-import { EntityViewTab, getRunTabs } from '@/src/utils/tabs/utils';
-import TabsContent from './TabsContent';
+import { formatDateTimeToLocalString } from '@/src/utils/formatting/date';
+import { onOpenInNewTab } from '@/src/utils/open-in-new-tab';
+import AnalyticsTab from './Analytics';
 
 interface Props {
   run: Run;
@@ -22,22 +27,49 @@ interface Props {
 
 const RunView: FC<Props> = ({ run, onRemove }) => {
   const t = useI18n();
-  const tabs = getRunTabs(t).splice(1); // remove properties tab, todo: return it when it will be ready
-  const [activeTab, setActiveTab] = useState(tabs[0].id as EntityViewTab);
 
   const noop = useCallback(() => {}, []);
-  // todo: return original markup when properties tab will be ready
+
+  const headerPostfix = useMemo(() => {
+    return (
+      <>
+        {!!run?.startedAt && (
+          <LabelledText label={t(RunsI18nKey.StartTime)} text={formatDateTimeToLocalString(run?.startedAt)} />
+        )}
+        {!!run?.completedAt && (
+          <LabelledText label={t(RunsI18nKey.EndTime)} text={formatDateTimeToLocalString(run?.completedAt)} />
+        )}
+
+        {!!run?.status && (
+          <LabelledText label={t(EntityFieldsI18nKey.status)}>
+            <RunStatusComponent status={run.status} />
+          </LabelledText>
+        )}
+        {!!run?.testRunName && (
+          <DialLabelledText
+            label={t(RunsI18nKey.TestSuite)}
+            text={run.testSuiteId}
+            postfix={
+              <DialIconButton
+                className="text-secondary size-[20px]"
+                onClick={() => onOpenInNewTab(ApplicationRoute.TestSuites, { id: run.testSuiteId })}
+                icon={<IconExternalLink {...BASE_BUTTON_ICON_PROPS} />}
+              />
+            }
+          />
+        )}
+      </>
+    );
+  }, [run?.completedAt, run?.startedAt, run.status, run.testRunName, run.testSuiteId, t]);
+
   return (
     <div className="flex flex-col flex-1 min-h-0 w-full bg-layer-2 rounded p-4 pb-14 lg:pb-4 relative">
-      {/* <SimpleEntityHeader
+      <SimpleEntityHeader
         view={ApplicationRoute.Runs}
         entity={run}
         isChanged={false}
         onDiscard={noop}
         onSave={noop}
-        tabs={tabs}
-        activeTab={activeTab}
-        onChangeActiveTab={setActiveTab}
         onRemove={onRemove}
       >
         {run.grafanaExploreUrl && (
@@ -47,30 +79,13 @@ const RunView: FC<Props> = ({ run, onRemove }) => {
             onClick={() => window.open(run.grafanaExploreUrl, '_blank')}
           />
         )}
-      </SimpleEntityHeader> */}
+      </SimpleEntityHeader>
 
-      <RunHeader
-        view={ApplicationRoute.Runs}
-        entity={run as Entity}
-        tabs={tabs}
-        isChanged={false}
-        onDiscard={noop}
-        onSave={noop}
-        activeTab={activeTab}
-        onChangeActiveTab={setActiveTab}
-        onRemove={onRemove}
-      >
-        {run.grafanaExploreUrl && (
-          <DialLinkButton
-            iconBefore={<Grafana />}
-            label={t(RunsI18nKey.GrafanaRun)}
-            onClick={() => window.open(run.grafanaExploreUrl, '_blank')}
-          />
-        )}
-      </RunHeader>
-
-      <div className="flex-1 overflow-auto min-h-0">
-        <TabsContent run={run} activeTab={activeTab} />
+      <div className="flex flex-col flex-1 min-h-0">
+        <EntityInfoHeader postfix={headerPostfix} view={ApplicationRoute.Runs} />
+        <div className="flex-1 min-h-0 mt-8">
+          <AnalyticsTab run={run} />
+        </div>
       </div>
     </div>
   );
