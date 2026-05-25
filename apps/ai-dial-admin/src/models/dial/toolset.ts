eafa8c5c582eaee884c19ceb5174039a9d48601b
Yauheni Osipau <yauheni_osipau@epam.com>
2026-05-25 18:14:47 +0300

    fix: changed enum

--- apps/ai-dial-admin/src/models/dial/toolset.ts
+++ apps/ai-dial-admin/src/models/dial/toolset.ts
@@ -37,9 +37,9 @@ export interface ToolsetAuthSettings {
 }
 
 export enum TokenEndpointAuthMethod {
-  CLIENT_SECRET_POST = 'CLIENT_SECRET_POST',
-  CLIENT_SECRET_BASIC = 'CLIENT_SECRET_BASIC',
-  CLIENT_SECRET_NONE = 'NONE',
+  CLIENT_SECRET_POST = 'client_secret_post',
+  CLIENT_SECRET_BASIC = 'client_secret_basic',
+  CLIENT_SECRET_NONE = 'none',
 }
 
 export enum ToolsetCodeChallengeMethod {
