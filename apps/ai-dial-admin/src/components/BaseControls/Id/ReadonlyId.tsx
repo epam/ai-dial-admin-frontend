b0a4ab86b1e3c480b420548031e3b8de073da57b
Kamianchuk <aliaksandr_kamianchuk@epam.com>
2026-06-17 11:08:11 +0300

    fix: fixed styles

--- apps/ai-dial-admin/src/components/BaseControls/Id/ReadonlyId.tsx
+++ apps/ai-dial-admin/src/components/BaseControls/Id/ReadonlyId.tsx
@@ -18,7 +18,7 @@ const ReadonlyId: FC<Props> = ({ value }) => {
     <div className="flex-1 min-w-0">
       <LabelledText copyLabel={t(EntityFieldsI18nKey.id)} copyable={true}>
         <div className="flex flex-row gap-x-3 items-center max-w-[640px]">
-          <DialEllipsisTooltip contentClassName="truncate" text={value || ''} />
+          <DialEllipsisTooltip className="flex-0" text={value || ''} />
           <CopyButton value={value || ''} valueLabel={t(EntityFieldsI18nKey.id)} />
         </div>
       </LabelledText>
