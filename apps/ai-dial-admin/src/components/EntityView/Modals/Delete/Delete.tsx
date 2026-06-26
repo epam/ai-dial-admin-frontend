31ede161c85dfb2ebeb61c00d83c76352f035544
Aliaksandr Kamianchuk <144103068+Kamianchuk@users.noreply.github.com>
2026-06-26 13:20:32 +0300

    fix: assets conversations delete functionality failed when deletion from properties tab (Issue #3750) (#3759)

--- apps/ai-dial-admin/src/components/EntityView/Modals/Delete/Delete.tsx
+++ apps/ai-dial-admin/src/components/EntityView/Modals/Delete/Delete.tsx
@@ -151,10 +151,6 @@ const DeleteConfirmationModal = <T extends Artefact>({
             router.push(view);
           }
           router.refresh();
-        } else if (isSelectedView) {
-          onCloseModal();
-          router.push(view);
-          router.refresh();
         }
       })
       .catch((error) => {
