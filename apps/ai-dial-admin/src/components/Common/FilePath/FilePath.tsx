372a5aff6791055db7f7054ceb856c416a2d5f5e
Yauheni Osipau <yauheni_osipau@epam.com>
2026-09-11 15:21:09 +0300

    fix: added correct bucket fetch

--- apps/ai-dial-admin/src/components/Common/FilePath/FilePath.tsx
+++ apps/ai-dial-admin/src/components/Common/FilePath/FilePath.tsx
@@ -10,15 +10,16 @@ import {
 } from '@epam/ai-dial-ui-kit';
 import { IconFolderShare } from '@tabler/icons-react';
 
+import { getParentPathByFullPath } from '@/src/components/Assets/utils';
+import { ROOT_FOLDER } from '@/src/constants/file';
 import { ActionMenuOperationI18nKey } from '@/src/constants/i18n';
 import { BASE_BUTTON_ICON_PROPS, CONTROL_WITH_BUTTON_WIDTH } from '@/src/constants/main-layout';
 import { AssetsFolderContext } from '@/src/context/assets/AssetsFolderContext';
 import { useI18n } from '@/src/locales/client';
-import { ROOT_FOLDER } from '@/src/constants/file';
 import { ServerActionResponse } from '@/src/models/server-action';
-import { getParentPathByFullPath } from '@/src/components/Assets/utils';
-import { excludePlatformRoot, getFilePathGridOptions, processAssetsData } from './utils';
 import { ApplicationRoute } from '@/src/types/routes';
+import { getRootFolders } from '@/src/utils/files/root-folder';
+import { excludePlatformRoot, getFilePathGridOptions, processAssetsData } from './utils';
 
 interface Props {
   label: string;
@@ -53,12 +54,12 @@ const FilePath: FC<Props> = ({
   const [path, setPath] = useState(`${ROOT_FOLDER}/`);
 
   useEffect(() => {
-    const rootPath = `${ROOT_FOLDER}/`;
-    if (!loadedPaths.has(rootPath)) {
-      fetchFiles?.(rootPath);
-      setLoadedPaths((prev) => new Set(prev).add(rootPath));
+    const rootPaths = getRootFolders(view as ApplicationRoute).map((root) => `${root}/`);
+    if (rootPaths.some((path) => !loadedPaths.has(path)) && fetchFiles) {
+      fetchFiles(rootPaths.length > 1 ? rootPaths : rootPaths[0]);
+      setLoadedPaths(new Set(rootPaths));
     }
-  }, [fetchFiles, loadedPaths]);
+  }, [fetchFiles, loadedPaths, view]);
 
   useEffect(() => {}, [files]);
 
