b9fc8a849c3c08fe85196257fdb8100790bcdcad
Yauheni Osipau <yauheni_osipau@epam.com>
2026-09-14 10:30:52 +0300

    fix: added colors fallback

--- apps/ai-dial-admin/src/utils/themes/apply-theme-colors.ts
+++ apps/ai-dial-admin/src/utils/themes/apply-theme-colors.ts
@@ -1,14 +1,14 @@
 import { Theme } from '@/src/models/theme';
 import { setToLocalStorage } from '@/src/utils/local-storage';
+import { fallbackDarkTheme } from './constant';
 
 export const applyThemeColors = (div: HTMLElement, theme?: Theme) => {
-  if (theme) {
-    const themeColors = theme.colors;
-    div.removeAttribute('style'); // temporary (dark and white theme colors are not synchronized)
-    Object.entries(themeColors).forEach(([key, value]) => {
-      div.style.setProperty(`--${key}`, value);
-    });
+  div.removeAttribute('style'); // temporary (dark and white theme colors are not synchronized)
 
+  Object.entries(theme ? theme.colors : fallbackDarkTheme).forEach(([key, value]) => {
+    div.style.setProperty(`--${key}`, value);
+  });
+  if (theme) {
     setToLocalStorage('theme', theme.id); // Persist the theme
   }
 };
