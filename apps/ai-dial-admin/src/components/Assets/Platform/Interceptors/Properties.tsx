cab3f1aefbaaf2e6703971544ffee77e53c262b4
Yauheni Osipau <yauheni_osipau@epam.com>
2026-09-23 21:31:53 +0300

    fix: added features selection based on route

--- apps/ai-dial-admin/src/components/Assets/Platform/Interceptors/Properties.tsx
+++ apps/ai-dial-admin/src/components/Assets/Platform/Interceptors/Properties.tsx
@@ -51,6 +51,7 @@ const InterceptorAssetProperties: FC<Props> = ({ asset, catalogSchemas, onChange
           onChangeInterfaces={(interfaces) => onChange({ ...asset, interfaces })}
           allowedTypes={INTERCEPTOR_INTERFACE_TYPES}
           entityBaseUrl={asset.baseUrl}
+          view={ApplicationRoute.PlatformInterceptors}
           isAsset
         />
         <EndpointControl
